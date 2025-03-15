ac3c3568214d3fcbfbd3192e5eba86de4f76daa7
mchernova <mchernova3706@gmail.com>
2025-03-15 14:21:45 +0300

    Change name. Comment import .WebElement

--- src/test/java/school/redrover/StarshipTroopersTest.java
+++ src/test/java/school/redrover/StarshipTroopersTest.java
@@ -3,7 +3,7 @@ package school.redrover;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+//import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterMethod;
