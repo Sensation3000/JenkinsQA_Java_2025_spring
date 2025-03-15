caca53a008d48f9a3d214632f5538e7fd9f5c3d4
NatTestCode <natashakoroleva2022@gmail.com>
2025-03-15 18:35:28 -0400

    Added KorolevaTest.java file (first test example)

---
+++ src/test/java/school/redrover/KorolevaTest.java
@@ -0,0 +1,30 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class KorolevaTest {
+    @Test
+    public void testStNina() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://stninaorlando.org");
+
+        WebElement input = driver.findElement(By.xpath("//input[@class='required email']"));
+        input.sendKeys("natasha");
+
+        Thread.sleep(1000);
+
+        WebElement button = driver.findElement(By.xpath("//input[@id='mc-embedded-subscribe']"));
+        button.click();
+
+        WebElement page = driver.findElement(By.xpath("//div[@class='formstatus error']"));
+        String pageText = page.getText();
+        Assert.assertEquals(pageText,"There are errors below");
+
+        driver.quit();
+    }
+}
