69510ed8ed5835a2a91f688ad9cce29d1d26dc7e
PavelV <202444749+pvel-AQA@users.noreply.github.com>
2025-03-15 14:13:21 +0300

    first commit of the group

---
+++ src/test/java/school/redrover/GroupAQARookies.java
@@ -0,0 +1,35 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+public class GroupAQARookies {
+
+    @Test
+    public void testOnliner() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.onliner.by/");
+        String title = driver.getTitle();
+        Assert.assertEquals(title, "Onlíner");
+
+        WebElement textBox = driver.findElement(By.xpath("//*[@id=\"fast-search\"]/div/input"));
+        textBox.sendKeys("Iphone");
+
+        WebElement iframe = driver.findElement(By.cssSelector("iframe.modal-iframe"));
+        driver.switchTo().frame(iframe);
+        Thread.sleep(2000);
+
+        WebElement iphoneLink = driver.findElement(By.xpath("//a[contains(text(), 'Телефон Apple iPhone 16e 128GB (белый)')]"));
+        iphoneLink.click();
+
+        String title2 = driver.getTitle();
+        Assert.assertEquals(title2, "iPhone 16e 128GB белый (Айфон 16е) купить в Минске");
+
+        driver.quit();
+    }
+
+}
