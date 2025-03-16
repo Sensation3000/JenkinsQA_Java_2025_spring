e92e742b928f0f585f7f0228f5c26e680fe0abfd
Anastasia Bodunova <anprado21@gmail.com>
2025-03-16 14:49:03 +0100

    Conflict test 2

--- src/test/java/school/redrover/GroupRedRoverJavaUTC3Test.java
+++ src/test/java/school/redrover/GroupRedRoverJavaUTC3Test.java
@@ -85,7 +85,7 @@ public class GroupRedRoverJavaUTC3Test {
     }
 
     @Test
-    public void RickAstleyTest() {
+    public void RickAstleyTest() throws InterruptedException {
 
         String xPathPlayButton = "//button[@aria-keyshortcuts='k']";
         String xPathReject = "//button[contains(@aria-label, 'Reject the use of cookies')]";
@@ -105,6 +105,7 @@ public class GroupRedRoverJavaUTC3Test {
         WebElement button = getWait10().until(ExpectedConditions.elementToBeClickable(By.xpath(xPathPlayButton)));
 
         for (int i = 0; i < 10; i++) {
+            Thread.sleep(8000);
             button.click();
             count++;
         }
