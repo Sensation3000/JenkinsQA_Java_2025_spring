72614e3fea5464609989241fa8fb4ae43e4481fd
Dmitry Falin <dfalin1993@gmail.com>
2025-03-16 12:01:29 +0300

    bonigarcia Web Form test

--- src/test/java/school/redrover/GroupCodeCraftTest.java
+++ src/test/java/school/redrover/GroupCodeCraftTest.java
@@ -1,16 +1,21 @@
 package school.redrover;
 
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
+import java.io.File;
+import java.io.IOException;
 import java.time.Duration;
 
 import static org.testng.Assert.*;
@@ -26,7 +31,7 @@ public class GroupCodeCraftTest {
     }
 
     @AfterMethod
-    public void teardown() {
+    public void tearDown() {
         if (driver != null) {
             driver.quit();
         }
@@ -202,6 +207,160 @@ public class GroupCodeCraftTest {
         assertEquals(eMailWeb.getText(), "Email:" + eMail);
         assertEquals(currentAddressWeb.getText(), "Current Address :" + currentAddress);
         assertEquals(permAddressWeb.getText(), "Permananet Address :" + permAddress);
+
+    }
+
+    @Test
+    public void bonigarciaWebForm() throws InterruptedException, IOException {
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+
+        driver.get("https://bonigarcia.dev/selenium-webdriver-java/web-form.html");
+
+        driver.findElement(By.id("my-text-id")).sendKeys("aS1!@%&");
+        driver.findElement(By.xpath("//input[@type='password']"))
+                .sendKeys("aS1!@%&");
+        driver.findElement(By.tagName("textarea")).sendKeys("aS1!@%&");
+
+        boolean inputDisabledCheck = driver.findElement
+                (By.xpath("//input[@name='my-disabled']")).isEnabled();
+
+        assertFalse(inputDisabledCheck);
+
+        assertNotNull(driver.findElement
+                        (By.xpath("//input[@name='my-readonly']")).
+                getDomAttribute("readonly"));
+
+        WebElement dropDownMenu = driver.findElement
+                (By.xpath("//select[@name='my-select']"));
+
+        Select select = new Select(dropDownMenu);
+
+        select.selectByContainsVisibleText("Open this select menu");
+        select.selectByValue("1");
+        select.selectByIndex(2);
+        select.selectByContainsVisibleText("Three");
+
+        assertEquals(driver.findElement
+                (By.xpath("//option[text()='Three']")).getText(), "Three");
+
+        WebElement dropDownMenuDataList = wait.until
+                (ExpectedConditions.elementToBeClickable
+                        (By.xpath("//input[@name='my-datalist']")));
+        dropDownMenuDataList.click();
+
+        ((JavascriptExecutor) driver).executeScript
+                ("arguments[0].value = 'San Francisco';", dropDownMenuDataList);
+        dropDownMenuDataList.clear();
+        dropDownMenuDataList.click();
+        ((JavascriptExecutor) driver).executeScript
+                ("arguments[0].value = 'New York';", dropDownMenuDataList);
+        dropDownMenuDataList.clear();
+        dropDownMenuDataList.click();
+        ((JavascriptExecutor) driver).executeScript
+                ("arguments[0].value = 'Seattle';", dropDownMenuDataList);
+        dropDownMenuDataList.clear();
+        dropDownMenuDataList.click();
+        ((JavascriptExecutor) driver).executeScript
+                ("arguments[0].value = 'Los Angeles';", dropDownMenuDataList);
+        dropDownMenuDataList.clear();
+        dropDownMenuDataList.click();
+        ((JavascriptExecutor) driver).executeScript
+                ("arguments[0].value = 'Chicago';", dropDownMenuDataList);
+
+        WebElement fileInput = driver.findElement(By.xpath("//input[@name='my-file']"));
+
+        // Создание временного txt-фаила для того, чтобы тест проходил у всех
+        // После прогона файл удаляется
+        File tempFile = File.createTempFile("temp", ".txt");
+        FileUtils.writeStringToFile(tempFile, "This is a test file content.", "UTF-8");
+        fileInput.sendKeys(tempFile.getAbsolutePath());
+        tempFile.deleteOnExit();
+
+        WebElement checkedCheckbox = wait.until
+                (ExpectedConditions.elementToBeClickable(By.xpath("//input[@id='my-check-1']")));
+        WebElement defaultCheckbox = wait.until
+                (ExpectedConditions.elementToBeClickable(By.xpath("//input[@id='my-check-2']")));
+        WebElement checkedRadio = wait.until
+                (ExpectedConditions.elementToBeClickable(By.xpath("//input[@id='my-radio-1']")));
+        WebElement defaultRadio = wait.until
+                (ExpectedConditions.elementToBeClickable(By.xpath("//input[@id='my-radio-2']")));
+
+        checkedCheckbox.click();
+
+        assertFalse(checkedCheckbox.isSelected());
+        checkedCheckbox.click();
+        assertTrue(checkedCheckbox.isSelected());
+
+        defaultCheckbox.click();
+        assertTrue(defaultCheckbox.isSelected());
+        defaultCheckbox.click();
+        assertFalse(defaultCheckbox.isSelected());
+
+        assertTrue(checkedRadio.isSelected());
+        Thread.sleep(100);
+        defaultRadio.click();
+        assertFalse(checkedRadio.isSelected());
+        assertTrue(defaultRadio.isSelected());
+
+        WebElement colorPicker = driver.findElement
+                (By.xpath("//input[@name='my-colors']"));
+
+        colorPicker.click();
+
+        String currentColor = colorPicker.getDomAttribute("value");
+        assertEquals(currentColor, "#563d7c");
+
+        JavascriptExecutor js = (JavascriptExecutor) driver;
+        js.executeScript("arguments[0].value = '#C47A12';", colorPicker);
+        // Я так особо и не разобрался как с Color Picker работать, даже нейронки особо не помогают
+
+        WebElement dateField = driver.findElement(By.xpath("//input[@name='my-date']"));
+        dateField.click();
+
+        WebElement previousMonth = wait.until
+                (ExpectedConditions.visibilityOfElementLocated(By.xpath("//th[@class='prev']")));
+
+        for (int i = 0; i < 5; i++) {
+            previousMonth.click();
+        }
+        WebElement monthAndYear = wait.until
+                (ExpectedConditions.visibilityOfElementLocated
+                        (By.xpath("//div[@class='datepicker-days']/descendant::th[@class='datepicker-switch']")));
+        assertEquals(monthAndYear.getText(), "October 2024");
+
+        WebElement nextMonth = wait.until
+                (ExpectedConditions.visibilityOfElementLocated
+                        (By.xpath("//div[@class='datepicker-days']/descendant::th[@class='next']")));
+
+        for (int i = 0; i < 5; i++) {
+            nextMonth.click();
+        }
+        assertEquals(monthAndYear.getText(), "March 2025");
+
+        WebElement dayPick = wait.until
+                (ExpectedConditions.visibilityOfElementLocated
+                        (By.xpath("//div[@class='datepicker-days']/descendant::td[text()='2' and @class='day']")));
+
+        dayPick.click();
+        monthAndYear.click();
+
+        WebElement monthClick = wait.until
+                (ExpectedConditions.visibilityOfElementLocated
+                        (By.xpath("//span[text()='Jun']")));
+        monthClick.click();
+
+        defaultRadio.click();
+
+        WebElement range = driver.findElement(By.xpath("//input[@name='my-range']"));
+
+        for (int i = 0; i < 11; i++) {
+            String x = String.valueOf(i);
+            js.executeScript("arguments[0].value = '" + x + "';", range);
+        }
+
+        WebElement submit = driver.findElement(By.tagName("button"));
+        submit.click();
+        driver.navigate().back();
 
     }
 }
