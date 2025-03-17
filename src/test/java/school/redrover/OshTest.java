1a33cca4506d40157936d00d1f104b36a82cb326
o.sheresh <o.shereshqa@gmail.com>
2025-03-17 14:15:06 +0300

    OSheresh_test_hw13

---
+++ src/test/java/school/redrover/OshTest.java
@@ -0,0 +1,41 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertEquals;
+
+public class OshTest {
+    @Test
+    public void OshTest() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        Actions action = new Actions(driver);
+
+        driver.get("https://learningenglish.voanews.com/p/5610.html");
+
+
+        driver.findElement(By.xpath("//*[@id=\"page\"]/div[1]/div/div/div[1]/label[2]")).click();
+
+
+        WebElement textBox = driver.findElement(By.xpath("/html/body/div[1]/div/div[1]/div/div/div[1]/div[2]/div/form/input"));
+        textBox.sendKeys("learning english");
+
+        Thread.sleep(1000);
+
+        WebElement searchButton = driver.findElement(By.xpath("//*[@id=\"form-topSearchHeader\"]/button"));
+        searchButton.click();
+
+        Thread.sleep(1000);
+
+        WebElement title = driver.findElement(By.xpath("//*[@id=\"search-results\"]/div[2]/div/ul/li[1]/div/div/a/h4"));
+        String titleText = title.getText();
+
+        assertEquals(titleText, "Learning English Podcast");
+
+        driver.quit();
+    }
+}
