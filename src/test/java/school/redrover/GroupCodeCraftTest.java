b0f25659a6c00aa7a37ce287460f4c6ceac2be25
Olga <145039622+OlgaSheresh@users.noreply.github.com>
2025-03-17 20:37:09 +0300

    Add test (#81)

--- src/test/java/school/redrover/GroupCodeCraftTest.java
+++ src/test/java/school/redrover/GroupCodeCraftTest.java
@@ -742,5 +742,32 @@ public class GroupCodeCraftTest {
 
         Thread.sleep(2000);
     }
+    @Test
+    public void LearningEnglish() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
 
+
+        driver.get("https://learningenglish.voanews.com/p/5610.html");
+
+
+        driver.findElement(By.xpath("//*[@id=\"page\"]/div[1]/div/div/div[1]/label[2]")).click();
+
+
+        WebElement textBox = driver.findElement(By.xpath("/html/body/div[1]/div/div[1]/div/div/div[1]/div[2]/div/form/input"));
+        textBox.sendKeys("learning english");
+
+        Thread.sleep(1000);
+
+        WebElement searchButton = driver.findElement(By.xpath("//*[@id=\"form-topSearchHeader\"]/button"));
+        searchButton.click();
+
+        Thread.sleep(1000);
+
+        WebElement title = driver.findElement(By.xpath("//*[@id=\"search-results\"]/div[2]/div/ul/li[1]/div/div/a/h4"));
+        String titleText = title.getText();
+
+        assertEquals(titleText, "Learning English Podcast");
+
+        driver.quit();
+    }
 }
