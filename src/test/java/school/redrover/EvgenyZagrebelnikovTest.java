dcee2d97d45fac4f3e317fdf71cc933c3dee0abc
EvgenyZagrebelnikov <ezagrebelnikov@gmail.com>
2025-03-19 02:08:06 +0700

    Edited the names of the locators in the first test. Added a new test on the new site.

--- src/test/java/school/redrover/EvgenyZagrebelnikovTest.java
+++ src/test/java/school/redrover/EvgenyZagrebelnikovTest.java
@@ -14,9 +14,9 @@ public class EvgenyZagrebelnikovTest {
         WebDriver driver = new ChromeDriver();
         driver.get("https://world-weather.ru/pogoda/");
 
-        WebElement documentationButton = driver.findElement(By.xpath("//*[@id=\"meny\"]/li[2]/a"));
-        Thread.sleep(4000);
-        documentationButton.click();
+        WebElement archiveButton = driver.findElement(By.xpath("//*[@id=\"meny\"]/li[2]/a"));
+        Thread.sleep(2000);
+        archiveButton.click();
 
         WebElement textTitle = driver.findElement(By.xpath("//*[@id=\"content-left\"]/h1"));
         String text = textTitle.getText();
@@ -24,5 +24,36 @@ public class EvgenyZagrebelnikovTest {
         Assert.assertEquals(text, "Архив погоды в мире");
 
         driver.quit();
+    }
+
+    @Test
+    public void testSearchAndSelectCity() throws InterruptedException {
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://openweathermap.org/");
+
+        WebElement searchInputFieldAndEnterCityName = driver.findElement(By.xpath("//*[@id=" +
+                "\"weather-widget\"]/div[2]/div/div/div[2]/div[1]/div/input"));
+        searchInputFieldAndEnterCityName.sendKeys("London");
+
+        WebElement searchButtonClick = driver.findElement(By.xpath("//*[@id=\"weather-widget\"]" +
+                "/div[2]/div/div/div[2]/div[1]/button"));
+        searchButtonClick.click();
+        Thread.sleep(2000);
+
+        WebElement selectCityFromList = driver.findElement(By.xpath("//*[@id=\"weather-widget\"]/div[2]" +
+                "/div/div/div[2]/div[1]/div/ul/li[2]"));
+        Thread.sleep(1000);
+        selectCityFromList.click();
+        Thread.sleep(2000);
+
+        WebElement checkCityTitle = driver.findElement(By.xpath("//*[@id='weather-widget']" +
+                "/div[3]/div[1]/div[1]/div[1]/h2"));
+        String text = checkCityTitle.getText();
+
+        Assert.assertEquals(text, "London, CA");
+        Thread.sleep(1000);
+
+        driver.quit();
+
     }
 }
