5897b169c6f16a0d36072217bb25c3fead0ee3cb
natalia burtnyk <nataliaburtnyk@gmail.com>
2025-03-21 12:45:30 -0500

    Class name change (approved by lead)

--- src/test/java/school/redrover/AGroupJavaQATest.java
+++ src/test/java/school/redrover/AGroupJavaQATest.java
@@ -19,7 +19,7 @@ import java.util.stream.Collectors;
 import static org.testng.Assert.assertEquals;
 import static org.testng.Assert.assertTrue;
 
-public class Group_JavaQATest {
+public class AGroupJavaQATest {
     By firstName = By.id("request-pricing_firstName");
     By lastName = By.id("request-pricing_lastName");
     By countryOfResidence = By.xpath("//*[@data-id='request-pricing_country']");
