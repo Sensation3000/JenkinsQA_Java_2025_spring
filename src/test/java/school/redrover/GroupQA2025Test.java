bbc69ae3db9695e7123fc54272f916776857abdd
Irina_D <bibber14@mail.ru>
2025-03-21 06:49:03 +0300

    add new test

--- src/test/java/school/redrover/GroupQA2025Test.java
+++ src/test/java/school/redrover/GroupQA2025Test.java
@@ -2,6 +2,7 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
@@ -10,6 +11,8 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.Test;
 import io.github.bonigarcia.wdm.WebDriverManager;
+
+import static org.testng.Assert.assertEquals;
 
 
 public class GroupQA2025Test {
@@ -41,7 +44,7 @@ public class GroupQA2025Test {
         driver.findElement(By.linkText("Sample App")).click();
         driver.findElement(By.xpath("//button[@class = 'btn btn-primary']")).click();
 
-        Assert.assertEquals(
+        assertEquals(
                 driver.findElement(By.xpath("//label[@class = 'text-danger']")).getText(),
                 "Invalid username/password"
         );
@@ -56,7 +59,7 @@ public class GroupQA2025Test {
         driver.findElement(By.xpath("//input[@name = 'UserName']")).sendKeys("UserName");
         driver.findElement(By.xpath("//button[@class = 'btn btn-primary']")).click();
 
-        Assert.assertEquals(
+        assertEquals(
                 driver.findElement(By.xpath("//label[@class = 'text-danger']")).getText(),
                 "Invalid username/password"
         );
@@ -71,7 +74,7 @@ public class GroupQA2025Test {
         driver.findElement(By.xpath("//input[@name = 'Password']")).sendKeys("pwd");
         driver.findElement(By.xpath("//button[@class = 'btn btn-primary']")).click();
 
-        Assert.assertEquals(
+        assertEquals(
                 driver.findElement(By.xpath("//label[@class = 'text-danger']")).getText(),
                 "Invalid username/password"
         );
@@ -87,7 +90,7 @@ public class GroupQA2025Test {
         driver.findElement(By.xpath("//input[@name = 'Password']")).sendKeys("pwd");
         driver.findElement(By.xpath("//button[@class = 'btn btn-primary']")).click();
 
-        Assert.assertEquals(
+        assertEquals(
                 driver.findElement(By.xpath("//label[@class = 'text-success']")).getText(),
                 "Welcome, UserName!"
         );
@@ -98,5 +101,13 @@ public class GroupQA2025Test {
         driver.get("http://uitestingplayground.com/");
 
         Assert.assertTrue(driver.findElement(By.cssSelector("[href=\"/dynamicid\"]")).isEnabled());
+    }
+    @Test
+    public void getLinkName() {
+        driver.get("http://uitestingplayground.com/");
+
+        WebElement link = driver.findElement(By.cssSelector("[href=\"/dynamicid\"]"));
+        link.getText();
+        assertEquals(link.getText(),"Dynamic ID");
     }
 }
