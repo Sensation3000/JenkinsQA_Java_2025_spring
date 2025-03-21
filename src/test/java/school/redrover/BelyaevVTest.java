15cfd1639bb3a9537f3300cc55da5bc2c46eb405
apple <andreygrigo2002@gmail.com>
2025-03-20 18:38:19 -0700

    Add a change "Api1"

--- src/test/java/school/redrover/BelyaevVTest.java
+++ src/test/java/school/redrover/BelyaevVTest.java
@@ -22,7 +22,7 @@ public class BelyaevVTest {
         searchButton.click();
 
         WebElement searchText = driver.findElement(By.className("heading-search__input"));
-        searchText.sendKeys("Api");
+        searchText.sendKeys("Api1");
 
         searchButton.click();
 
