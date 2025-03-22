e9ff46e6b3f13ea298afcb5be55684c998a024f6
Greta Simonian <greta77762@gmail.com>
2025-03-21 21:16:53 -0700

    add swagLabTest

--- src/test/java/school/redrover/AGroupJavaQATest.java
+++ src/test/java/school/redrover/AGroupJavaQATest.java
@@ -175,4 +175,28 @@ public class AGroupJavaQATest {
 
         driver.quit();
     }
+
+    @Test
+    public void swagLabTest() {
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.saucedemo.com/");
+
+        driver.findElement(By.xpath("//input[@id = 'user-name']")).sendKeys("standard_user");
+        driver.findElement(By.xpath("//input[@id = 'password']")).sendKeys("secret_sauce");
+        driver.findElement(By.xpath("//form/input[@id = 'login-button']")).click();
+        driver.findElement(By.xpath("//button[@id = 'add-to-cart-sauce-labs-backpack']")).click();
+        driver.findElement(By.id("shopping_cart_container")).click();
+        driver.findElement(By.xpath("//button[@id = 'checkout']" )).click();
+        driver.findElement(By.xpath("//input[@id = 'first-name']")).sendKeys("Anna");
+        driver.findElement(By.xpath("//input[@id = 'last-name']")).sendKeys("Anina");
+        driver.findElement(By.xpath("//input[@id = 'postal-code']")).sendKeys("19283");
+        driver.findElement(By.xpath("//input[@id = 'continue'] ")).click();
+        driver.findElement(By.xpath("//button[@id= 'finish']")).click();
+        WebElement confirm = driver.findElement(By.xpath("//div/div/h2"));
+        String confirmText = confirm.getText();
+        assertEquals(confirmText, "Thank you for your order!");
+
+        driver.quit();
+    }
 }
