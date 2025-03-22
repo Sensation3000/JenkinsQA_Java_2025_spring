5ef3cbc8a7a7d07512acfc5d64488c7d824974eb
Katya <katerinagorba.qa@gmail.com>
2025-03-21 20:38:56 -0400

    Add new test

--- src/test/java/school/redrover/GroupJavaESTTest.java
+++ src/test/java/school/redrover/GroupJavaESTTest.java
@@ -3,6 +3,7 @@ package school.redrover;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
@@ -38,6 +39,23 @@ public class GroupJavaESTTest {
         driver.findElement(By.name("login-button")).click();
 
         assertTrue(driver.findElement(By.className("error-message-container")).isDisplayed());
+
+        driver.quit();
+    }
+
+    @Test
+    public void testLogin() {
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://www.saucedemo.com/");
+
+        WebElement textBox = driver.findElement(By.id("user-name"));
+        WebElement submitButton = driver.findElement(By.name("login-button"));
+        textBox.sendKeys("standard_user");
+
+        WebElement textBox2 = driver.findElement(By.id("password"));
+        textBox2.sendKeys("secret_sauce");
+        submitButton.click();
 
         driver.quit();
     }
