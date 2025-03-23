6db9a19e831806e05bbdf6708c77c9e97ac0b845
DaniilOznobikhin <DaniilOznobikhin@yandex.ru>
2025-03-23 14:23:38 +0300

    New try

--- src/test/java/school/redrover/GroupPushAndPrayTest.java
+++ src/test/java/school/redrover/GroupPushAndPrayTest.java
@@ -24,7 +24,6 @@ public class GroupPushAndPrayTest {
         WebElement knopka = driver.findElement(By.xpath("/html/body/div[2]/div[1]/div/div[1]/form/button"));
         knopka.click();
 
-
         String ssilka = driver.getCurrentUrl();
 
         Assert.assertEquals(ssilka, "https://www.goodfon.ru/search/?q=%D0%94%D0%B2%D0%BE%D1%80%D1%86%D0%BE%D0%B2%D0%B0%D1%8F+%D0%BF%D0%BB%D0%BE%D1%89%D0%B0%D0%B4%D1%8C");
