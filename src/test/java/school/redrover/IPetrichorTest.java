bd74b697778c440bef9618e95a49b76e9c07fab7
Petrichor <180253417+IrinaPetrichor@users.noreply.github.com>
2025-03-25 19:19:31 -0400

    IPetrichor Refactor Test (#331)
    * Refactor IPetrichorTest

--- src/test/java/school/redrover/IPetrichorTest.java
+++ src/test/java/school/redrover/IPetrichorTest.java
@@ -15,20 +15,23 @@ public class IPetrichorTest {
     private WebDriver driver;
 
     @BeforeMethod
-    private void createDriver() {
+    private void createDriverAndNavigate() {
         if(this.driver == null) {
             this.driver = new ChromeDriver();
         }
+
+        this.driver.get("https://blackboxpuzzles.workroomprds.com/puzzle31/");
     }
 
     @AfterMethod
     private void quitDriver() {
         if(this.driver != null) {
             this.driver.quit();
+            this.driver = null;
         }
     }
 
-    private static class Data {
+    private static final class Data {
         private static final String BUTTON_DEFAULT_COLOR = "rgba(247, 247, 247, 1)";
 
         private enum Lamp {
@@ -45,7 +48,7 @@ public class IPetrichorTest {
                 this.color = color;
             }
 
-            private static Lamp byId(String id) {
+            private static Lamp getById(String id) {
                 for (Lamp value : values()) {
                     if (value.id.equals(id)) {
                         return value;
@@ -57,26 +60,32 @@ public class IPetrichorTest {
     }
 
     @Test()
-    public void testHomepageElementColorsOnLoad() {
-        driver.get("https://blackboxpuzzles.workroomprds.com/puzzle31/");
-
+    public void testHomepageElementsCountOnLoad() {
         final List<WebElement> LAMPS_ELEMENTS = driver.findElements(By.cssSelector(".BigLamp"));
         final List<WebElement> BUTTONS_ELEMENTS = driver.findElements(By.cssSelector(".BigButton"));
 
         Assert.assertEquals(LAMPS_ELEMENTS.size(), 4);
         Assert.assertEquals(BUTTONS_ELEMENTS.size(), 9);
+    }
+
+    @Test(dependsOnMethods = "testHomepageElementsCountOnLoad")
+    public void testHomepageLampsColorsOnLoad() {
+        final List<WebElement> LAMPS_ELEMENTS = driver.findElements(By.cssSelector(".BigLamp"));
 
         LAMPS_ELEMENTS.forEach(element -> {
-            final String elementId = element.getDomAttribute("id");
+            String elementId = element.getDomAttribute("id");
             Assert.assertNotNull(elementId);
-
-            Data.Lamp lampDataId = Data.Lamp.byId(elementId);
-            if (lampDataId != null) {
-                Assert.assertEquals(element.getCssValue("background-color"), lampDataId.color);
-            } else {
-                Assert.fail("Lamp with id " + elementId + " not found.");
-            }
+            
+            Data.Lamp foundLampId = Data.Lamp.getById(elementId);
+            Assert.assertNotNull(foundLampId);
+            
+            Assert.assertEquals(element.getCssValue("background-color"), foundLampId.color);
         });
+    }
+
+    @Test(dependsOnMethods = "testHomepageElementsCountOnLoad")
+    public void testHomepageButtonsColorsOnLoad() {
+        final List<WebElement> BUTTONS_ELEMENTS = driver.findElements(By.cssSelector(".BigButton"));
 
         BUTTONS_ELEMENTS.forEach(element -> {
             Assert.assertEquals(element.getCssValue("background-color"), Data.BUTTON_DEFAULT_COLOR);
