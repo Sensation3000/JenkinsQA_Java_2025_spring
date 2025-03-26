ed3b809f680eca56b476dcc3b5ad80de0eeb3288
Tatyana <telcova@softmart.ru>
2025-03-26 13:46:37 +0300

    add test create new item

---
+++ src/test/java/school/redrover/CreateNewItem.java
@@ -0,0 +1,28 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.testng.annotations.Test;
+import school.redrover.common.BaseTest;
+import static org.testng.Assert.assertEquals;
+
+public class CreateNewItem extends BaseTest {
+
+    @Test
+    public void testCreateNewItem() throws InterruptedException {
+
+        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
+        getDriver().findElement(By.id("name")).sendKeys("Test item");
+        getDriver().findElement(By.xpath("//li[@class='hudson_model_FreeStyleProject']")).click();
+        getDriver().findElement(By.id("ok-button")).click();
+        getDriver().findElement(By.xpath("//input[@id='cb6']/following-sibling::label")).click();
+        getDriver().findElement(By.xpath("//button[@name='Submit']")).sendKeys(Keys.ENTER);
+        Thread.sleep(2000);
+        getDriver().findElement(By.xpath("//a[@href='/']")).click();
+
+        assertEquals(getDriver().findElement(By.xpath(
+                "//a[@href='job/Test%20item/']")).getText(),
+                "Test item"
+        );
+    }
+}
