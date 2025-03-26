536f90475939e8c9ba40230fc35004347542f1b2
R.O. <ramos16qa@gmail.com>
2025-03-26 11:44:14 -0700

    Added first Test

---
+++ src/test/java/school/redrover/WelcomPageTest.java
@@ -0,0 +1,15 @@
+package school.redrover;
+import org.openqa.selenium.By;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.common.BaseTest;
+
+public class WelcomPageTest extends BaseTest {
+
+    @Test
+    public void testWelcome(){
+
+        String text = getDriver().findElement(By.xpath("//h1")).getText();
+        Assert.assertEquals(text, "Welcome to Jenkins!");
+    }
+}
