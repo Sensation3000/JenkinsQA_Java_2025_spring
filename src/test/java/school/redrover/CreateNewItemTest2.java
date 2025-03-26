2947794e5e7b3752b0653a6088df9834f267273e
pavel-khalilov <pavel.khalilov.qa@gmail.com>
2025-03-26 17:46:53 -0400

    ignore testWelcome() because of flaky Error:    WelcomPageTest.testWelcome:12 » StaleElementReference stale element reference: stale element not found

--- src/test/java/school/redrover/CreateNewItemTest2.java
+++ src/test/java/school/redrover/CreateNewItemTest2.java
@@ -8,9 +8,9 @@ import school.redrover.common.BaseTest;
 
 import java.util.List;
 
-public class CreateJobTest extends BaseTest {
+public class CreateNewItemTest2 extends BaseTest {
     @Test
-    public void testNotCreateNewJobWithoutName() throws InterruptedException {
+    public void testNotCreateNewItemWithoutName() throws InterruptedException {
         getDriver().findElement(By.cssSelector("div section:nth-of-type(1) ul")).click();
         List<WebElement> itemList = getDriver().findElements(By.cssSelector("ul.j-item-options li"));
         WebElement errorMessage = getDriver().findElement(By.cssSelector("#itemname-required"));
