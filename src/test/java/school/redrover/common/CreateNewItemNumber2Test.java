407bf84c60e72f049d851ff631671d7cce6cab34
ViktorSmiryagin <smiryaginviktorwtj@gmail.com>
2025-03-27 20:00:34 +0200

    success_created_item_check

--- src/test/java/school/redrover/common/CreateNewItemNumber2Test.java
+++ src/test/java/school/redrover/common/CreateNewItemNumber2Test.java
@@ -13,15 +13,22 @@ public class CreateNewItemNumber2Test extends BaseTest {
 
     @Test
     public void testItemCreation_Success() {
-        getDriver().findElement(By.xpath("//a[@it='hudson.model.Hudson@71fdd4fe' and @data-task-success='Done.']")).click();
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(5)); // увеличил до 5 сек
 
-        getDriver().findElement(By.name("name")).sendKeys("TestItemBlaBla");
+        WebElement link = wait.until(ExpectedConditions.elementToBeClickable(
+                By.xpath("//a[@it='hudson.model.Hudson@71fdd4fe' and @data-task-success='Done.']")
+        ));
+        link.click();
+
+        getDriver().findElement(By.name("name")).sendKeys
+                ("TestItemBlaBla");
         getDriver().findElement(By.xpath("//span[text()='Freestyle project']")).click();
         getDriver().findElement(By.xpath("//button[contains(@class,'jenkins-button--primary')]")).click();
         getDriver().findElement(By.xpath("//button[@formnovalidate='formNoValidate']")).click();
 
-        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(2));
-        WebElement createdProjectLink = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@class='model-link' and contains(@href, '/job/TestItemBlaBla/')]")));
+        WebElement createdProjectLink = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//a[@class='model-link' and contains(@href, '/job/TestItemBlaBla/')]")
+        ));
 
         Assert.assertTrue(createdProjectLink.isDisplayed(), "Project link not found after creation.");
     }
