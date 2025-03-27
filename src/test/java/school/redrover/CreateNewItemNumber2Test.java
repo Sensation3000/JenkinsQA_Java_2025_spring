e5782e2825f6b659c0a93935a8d92324b974752d
ViktorSmiryagin <smiryaginviktorwtj@gmail.com>
2025-03-27 20:45:59 +0200

    success_created_item_check

--- src/test/java/school/redrover/CreateNewItemNumber2Test.java
+++ src/test/java/school/redrover/CreateNewItemNumber2Test.java
@@ -2,34 +2,29 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
-
-import java.time.Duration;
 
 public class CreateNewItemNumber2Test extends BaseTest {
 
     @Test
     public void testItemCreation_Success() {
-        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
-
-        WebElement link = wait.until(ExpectedConditions.elementToBeClickable(
-                By.xpath("//a[@it='hudson.model.Hudson@71fdd4fe' and @data-task-success='Done.']")
-        ));
-        link.click();
-
-        getDriver().findElement(By.name("name")).sendKeys
-                ("TestItemBlaBla");
+        getDriver().findElement(By.cssSelector("a.task-link.task-link-no-confirm[href='/view/all/newJob']")).click();
+        getDriver().findElement(By.name("name")).sendKeys("TestItemBlaBla");
         getDriver().findElement(By.xpath("//span[text()='Freestyle project']")).click();
         getDriver().findElement(By.xpath("//button[contains(@class,'jenkins-button--primary')]")).click();
         getDriver().findElement(By.xpath("//button[@formnovalidate='formNoValidate']")).click();
 
-        WebElement createdProjectLink = wait.until(ExpectedConditions.visibilityOfElementLocated(
-                By.xpath("//a[@class='model-link' and contains(@href, '/job/TestItemBlaBla/')]")
-        ));
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        WebElement createdProjectLink = getDriver().findElement(
+                By.cssSelector("a.model-link[href='/job/TestItemBlaBla/']")
+        );
 
         Assert.assertTrue(createdProjectLink.isDisplayed(), "Project link not found after creation.");
     }
