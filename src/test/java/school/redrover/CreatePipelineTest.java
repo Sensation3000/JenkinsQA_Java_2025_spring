fc9b44d9fd9faca06a0ee858a914845ebd30988b
alexshigaev <you@example.com>
2025-03-27 19:53:40 +0100

    refactoring code test PipelineTest

--- src/test/java/school/redrover/CreatePipelineTest.java
+++ src/test/java/school/redrover/CreatePipelineTest.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.openqa.selenium.JavascriptExecutor;
