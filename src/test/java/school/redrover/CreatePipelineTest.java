355cee8c035e4d55652dd0a9e1b3be6909de583a
alexshigaev <you@example.com>
2025-03-27 19:53:14 +0100

    refactoring code test PipelineTest

--- src/test/java/school/redrover/CreatePipelineTest.java
+++ src/test/java/school/redrover/CreatePipelineTest.java
@@ -17,25 +17,27 @@ public class CreatePipelineTest extends BaseTest {
     public void testCreatePipeline() {
         WebDriver driver = getDriver();
 
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        new WebDriverWait(driver, Duration.ofSeconds(10))
+                .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@href='/view/all/newJob']")))
+                .click();
 
-        WebElement newItem = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@href='/view/all/newJob']")));
-        newItem.click();
-        driver.findElement(By.xpath("//input[@id='name']")).sendKeys("New Item");
+        driver.findElement(By.xpath("//input[@id='name']")).sendKeys("New2 Item");
         driver.findElement(By.xpath("//span[normalize-space()='Pipeline']")).click();
         driver.findElement(By.xpath("//button[@id='ok-button']")).click();
         driver.findElement(By.xpath("//textarea[@name='description']")).sendKeys("New pipeline");
         driver.findElement(By.xpath("//label[normalize-space()='GitHub project']")).click();
         driver.findElement(By.xpath("//input[@name='_.projectUrlStr']")).sendKeys
-        ("https://github.com/RedRoverSchool/JenkinsQA_Java_2025_spring.git");
+                ("https://github.com/RedRoverSchool/JenkinsQA_Java_2025_spring.git");
 
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView({block: 'center'});", 
-            driver.findElement(By.xpath("//label[contains(text(), 'GitHub hook trigger')]")));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView({block: 'center'});",
+                driver.findElement(By.xpath("//label[contains(text(), 'GitHub hook trigger')]")));
 
         driver.findElement(By.xpath("//label[contains(text(), 'GitHub hook trigger')]")).click();
         driver.findElement(By.xpath("(//select[@class='jenkins-select__input dropdownList'])[2]")).click();
         driver.findElement(By.xpath("//*[@id='breadcrumbs']/li[1]/a")).click();
 
-        Assert.assertTrue(driver.findElement(By.xpath("//a//span[contains(text(),'New Item')]")).isDisplayed());
+        Assert.assertEquals(driver.findElement(By.xpath("//span[contains(text(),'New Item')]")).getText(), 
+                "New Item", "Текст элемента не соответствует ожидаемому значению.");
+
     }
 }
