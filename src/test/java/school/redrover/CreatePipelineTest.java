aaf583c85b6f9e19c402e1ff8c147a752f82eca2
alexshigaev <you@example.com>
2025-03-27 20:02:28 +0100

    refactoring code test PipelineTest

--- src/test/java/school/redrover/CreatePipelineTest.java
+++ src/test/java/school/redrover/CreatePipelineTest.java
@@ -20,13 +20,12 @@ public class CreatePipelineTest extends BaseTest {
                 .until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@href='/view/all/newJob']")))
                 .click();
 
-        driver.findElement(By.xpath("//input[@id='name']")).sendKeys("New2 Item");
+        driver.findElement(By.xpath("//input[@id='name']")).sendKeys("New Item");
         driver.findElement(By.xpath("//span[normalize-space()='Pipeline']")).click();
         driver.findElement(By.xpath("//button[@id='ok-button']")).click();
         driver.findElement(By.xpath("//textarea[@name='description']")).sendKeys("New pipeline");
         driver.findElement(By.xpath("//label[normalize-space()='GitHub project']")).click();
-        driver.findElement(By.xpath("//input[@name='_.projectUrlStr']")).sendKeys
-                ("https://github.com/RedRoverSchool/JenkinsQA_Java_2025_spring.git");
+        driver.findElement(By.xpath("//input[@name='_.projectUrlStr']")).sendKeys("https://github.com/RedRoverSchool/JenkinsQA_Java_2025_spring.git");
 
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView({block: 'center'});",
                 driver.findElement(By.xpath("//label[contains(text(), 'GitHub hook trigger')]")));
@@ -35,8 +34,7 @@ public class CreatePipelineTest extends BaseTest {
         driver.findElement(By.xpath("(//select[@class='jenkins-select__input dropdownList'])[2]")).click();
         driver.findElement(By.xpath("//*[@id='breadcrumbs']/li[1]/a")).click();
 
-        Assert.assertEquals(driver.findElement(By.xpath("//span[contains(text(),'New Item')]")).getText(), 
-                "New Item", "Текст элемента не соответствует ожидаемому значению.");
-
+        Assert.assertEquals(driver.findElement(By.xpath("//a[contains(@class, 'model-link')]//span[contains(text(), 'New Item')]")).getText(),
+                "New Item");
     }
 }
