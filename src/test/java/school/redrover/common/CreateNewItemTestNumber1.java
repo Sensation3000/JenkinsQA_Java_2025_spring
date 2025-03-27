1dd2fa2b8962df712aeda1884958a36c71be58fe
ViktorSmiryagin <smiryaginviktorwtj@gmail.com>
2025-03-27 19:20:44 +0200

    success_created_item_check

---
+++ src/test/java/school/redrover/common/CreateNewItemTestNumber1.java
@@ -0,0 +1,27 @@
+package school.redrover.common;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.time.Duration;
+
+public class createNewItemNumber1Test extends BaseTest {
+
+    @Test
+    public void testItemCreation_Success() {
+        getDriver().findElement(By.xpath("//a[@it='hudson.model.Hudson@71fdd4fe']")).click();
+        getDriver().findElement(By.name("name")).sendKeys("TestItemBlaBla");
+        getDriver().findElement(By.xpath("//span[text()='Freestyle project']")).click();
+        getDriver().findElement(By.xpath("//button[contains(@class,'jenkins-button--primary')]")).click();
+        getDriver().findElement(By.xpath("//button[@formnovalidate='formNoValidate']")).click();
+
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(2));
+        WebElement createdProjectLink = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//a[@class='model-link' and contains(@href, '/job/TestItemBlaBla/')]")));
+
+        Assert.assertTrue(createdProjectLink.isDisplayed(), "Project link not found after creation.");
+    }
+}
