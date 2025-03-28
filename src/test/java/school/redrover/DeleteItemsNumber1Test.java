2fc2358dbbef3ad206f14caf9d9883815652df8d
ViktorSmiryagin <smiryaginviktorwtj@gmail.com>
2025-03-28 15:58:23 +0200

    success_created_item_check

--- src/test/java/school/redrover/DeleteItemsNumber1Test.java
+++ src/test/java/school/redrover/DeleteItemsNumber1Test.java
@@ -7,15 +7,18 @@ import org.testng.annotations.Test;
 
 import java.time.Duration;
 
-    public class DeleteItemsNumber1Test extends CreateNewItemNumber2Test {
-        @Test
-        public void testItemDelete(){
-            testItemCreation_Success();{
-                getDriver().findElement(By.xpath("//span[normalize-space(text())='Delete Project']")).click();
-                WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
-                wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@data-id='ok']")));
-                getDriver().findElement(By.xpath("//button[@data-id='ok']")).click();
-            }
-        }
+public class DeleteItemsNumber1Test extends CreateNewItemNumber2Test {
+    @Test
+    public void testItemDelete(){
+        testItemCreation_Success();
+        getDriver().findElement(By.xpath("//span[normalize-space(text())='Delete Project']")).click();
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@data-id='ok']")));
+        getDriver().findElement(By.xpath("//button[@data-id='ok']")).click();
+        boolean isProjectDeleted = getDriver().findElements(By.xpath("//a[contains(text(),'Имя_проекта')]")).isEmpty();
+        assert isProjectDeleted : "Проект не был удалён!";
     }
+}
+
+
 
