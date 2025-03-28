9a6af5d3be8cffb17379bb2fbb0ede77f002a4c0
ViktorSmiryagin <smiryaginviktorwtj@gmail.com>
2025-03-28 12:23:21 +0200

    success_created_item_check

--- src/test/java/school/redrover/DeleteItemsNumber1Test.java
+++ src/test/java/school/redrover/DeleteItemsNumber1Test.java
@@ -9,18 +9,14 @@ import java.time.Duration;
 
 public class DeleteItemsNumber1Test extends CreateNewItemNumber2Test {
     @Test
-    public void testItemDelete(){
-        testItemCreation_Success();{
-        getDriver().findElement(By.xpath("//span[normalize-space(text())='Delete Project']")).click();
-
-        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
-        wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@data-id='ok']")));
-
-        getDriver().findElement(By.xpath("//button[@data-id='ok']")).click();
-
-
+    public void testItemDelete() {
+        testItemCreation_Success();
+        {
+            getDriver().findElement(By.xpath("//span[normalize-space(text())='Delete Project']")).click();
+            WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
+            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[@data-id='ok']")));
+            getDriver().findElement(By.xpath("//button[@data-id='ok']")).click();
+        }
     }
-
-}
 }
 
