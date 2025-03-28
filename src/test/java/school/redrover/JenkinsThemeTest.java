ddb544b517f2f73e0e00010216883657bec36d34
Anonymous User <urodets@protonmail.com>
2025-03-28 14:55:29 +0300

    Add some new tests.

--- src/test/java/school/redrover/JenkinsThemeTest.java
+++ src/test/java/school/redrover/JenkinsThemeTest.java
@@ -3,11 +3,9 @@ package school.redrover;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
 
-@Ignore
 public class JenkinsThemeTest extends BaseTest {
 
         @Test
