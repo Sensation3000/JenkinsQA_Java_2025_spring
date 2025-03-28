c887411784c1507fcb2d2c887b7cd44ba42e5850
Dmitry Falin <dfalin1993@gmail.com>
2025-03-28 11:07:48 +0300

    add create folder test v1.4

---
+++ src/test/java/school/redrover/CreateFolderTest.java
@@ -0,0 +1,46 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+import school.redrover.common.BaseTest;
+
+import java.time.Duration;
+
+import static org.testng.Assert.assertEquals;
+
+public class CreateFolderTest extends BaseTest {
+
+    @Test
+    public void testCreateFolder () {
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
+        String folderName = "New Folder";
+
+        wait.until(ExpectedConditions.elementToBeClickable
+                        (By.xpath("//span[text()='New Item']/preceding-sibling::span")))
+                .click();
+
+        wait.until(ExpectedConditions.visibilityOfElementLocated(By.className("jenkins-input")))
+                .sendKeys(folderName);
+
+        WebElement folderBox = wait.until(ExpectedConditions.visibilityOfElementLocated
+                (By.xpath("//div[contains(text(), 'container')]")));
+        ((JavascriptExecutor) getDriver())
+                .executeScript("arguments[0].scrollIntoView(true);", folderBox);
+        folderBox.click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("ok-button")))
+                .click();
+
+        wait.until(ExpectedConditions.elementToBeClickable(By.cssSelector(".jenkins-submit-button ")))
+                .click();
+
+        String nameOfCreatedFolder = wait.until(ExpectedConditions.visibilityOfElementLocated
+                        (By.xpath("//h1[contains(text(), 'New Folder')]")))
+                .getText();
+
+        assertEquals(nameOfCreatedFolder, folderName);
+    }
+}
