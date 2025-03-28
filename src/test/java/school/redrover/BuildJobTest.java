b0c2d8796097058a4422831b33c3bf199f818ea0
Tatyana <telcova@softmart.ru>
2025-03-28 13:42:30 +0300

    add sleep 5000 sec

--- src/test/java/school/redrover/BuildJobTest.java
+++ src/test/java/school/redrover/BuildJobTest.java
@@ -39,7 +39,7 @@ public class BuildJobTest extends BaseTest {
         WebElement linkElement = getDriver().findElement(By.xpath("//a[@href='job/Test%20item/']//button[@class='jenkins-menu-dropdown-chevron']"));
         actions.moveToElement(linkElement).pause(500).click(linkElement).perform();
 
-        Thread.sleep(3000);
+        Thread.sleep(5000);
         getDriver().findElement(By.xpath("//button[contains(@href, 'build')]")).click();
         //getWait5().until(ExpectedConditions.elementToBeClickable(By.xpath("//button[contains(@href, 'build')]"))).click();
         getDriver().findElement(By.xpath("//tr[@id = 'job_Test item']//a[@href='job/Test%20item/']")).click();
