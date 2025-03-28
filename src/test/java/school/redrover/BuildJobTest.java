19faac0449d9179c97fcb35da50b6b13f422c4bc
Tatyana <telcova@softmart.ru>
2025-03-28 15:54:20 +0300

    waiter5

--- src/test/java/school/redrover/BuildJobTest.java
+++ src/test/java/school/redrover/BuildJobTest.java
@@ -40,9 +40,14 @@ public class BuildJobTest extends BaseTest {
 
         //WebElement buildNow = getDriver().findElement(By.xpath("//button[contains(@href, 'build')]"));
        // actions.moveToElement(buildNow).pause(1000).click(buildNow).perform();
-        Thread.sleep(1000);
+
+        //Thread.sleep(1000);
         //getDriver().findElement(By.xpath("//button[contains(@href, 'build')]")).click();
-        getDriver().findElement(By.cssSelector("button[href*='build']")).click();
+        //getDriver().findElement(By.cssSelector("button[href*='build']")).click();
+
+        getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//button[contains(@href, 'build')]"))).click();
+
+
 
         getDriver().findElement(By.xpath("//tr[@id = 'job_Test item']//a[@href='job/Test%20item/']")).click();
         Thread.sleep(1000);
