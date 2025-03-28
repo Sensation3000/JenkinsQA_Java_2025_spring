5416edf42f4da5d7e1b0aab46b72118aebb21d66
Марина Макеева <132477713+Marin4kin@users.noreply.github.com>
2025-03-28 18:52:09 +0300

    a few new sleeps (#410)
    * a few new sleeps
    
    * тест 28.3.25

--- src/test/java/school/redrover/JenkinsThemeTest.java
+++ src/test/java/school/redrover/JenkinsThemeTest.java
@@ -6,17 +6,16 @@ import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
 
-@Ignore
-
 public class JenkinsThemeTest extends BaseTest {
 
         @Test
-        public void testDarkTheme() {
+        public void testDarkTheme() throws InterruptedException {
             getDriver().findElement(By.xpath("//*[@id=\"page-header\"]/div[3]/a[1]/span")).click();
+            Thread.sleep(500);
             getDriver().findElement(By.xpath("//*[@id=\"tasks\"]/div[5]/span/a")).click();
             getDriver().findElement(By.xpath("//label[span[text() = 'Dark']]")).click();
             getDriver().findElement(By.cssSelector("button.jenkins-button.apply-button")).click();
-
+            Thread.sleep(400);
             Assert.assertEquals(getDriver().findElement(By.xpath(
                     "//*[@id=\"notification-bar\"]")).getText(),
                     "Saved");
