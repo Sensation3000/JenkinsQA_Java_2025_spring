586c2d1ff92ee6b736ae494b8b47d935b8b1ed5a
pavel-khalilov <pavel.khalilov.qa@gmail.com>
2025-03-29 17:25:19 -0400

    add test dark theme changing

--- src/test/java/school/redrover/ThemeChangingTest.java
+++ src/test/java/school/redrover/ThemeChangingTest.java
@@ -9,16 +9,17 @@ import school.redrover.common.BaseTest;
 public class ThemeChangingTest extends BaseTest {
 
     @Test
-    public void testDarkThemeSet() {
+    public void testDarkThemeSet() throws InterruptedException {
         getDriver().findElement(By.linkText("admin")).click();
         getDriver().findElement(By.linkText("Appearance")).click();
         getDriver().findElement(By.xpath("//input[@data-theme='dark']/ancestor::div[@class='app-theme-picker__item']")).click();
         getDriver().findElement(By.name("Submit")).click();
 
-        WebElement bodyColor = getDriver().findElement(By.tagName("body"));
-        WebElement textColor = getDriver().findElement(By.xpath("html[@data-theme='dark']"));
+        Thread.sleep(150);
 
-        Assert.assertEquals(bodyColor.getCssValue("background-color"), "rgba(31, 31, 35, 1)");
+        WebElement backgroundColor = getDriver().findElement(By.tagName("body"));
+        WebElement textColor = getDriver().findElement(By.xpath("html[@data-theme='dark']"));
+        Assert.assertEquals(backgroundColor.getCssValue("background-color"), "rgba(31, 31, 35, 1)");
         Assert.assertEquals(textColor.getCssValue("color"), "rgba(250, 250, 255, 1)");
     }
 }
