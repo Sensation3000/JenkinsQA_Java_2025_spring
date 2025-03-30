e69c881ecd3e812602f757b797c34277168c9531
Elena-Mansurova <eshkin.cot88@gmail.com>
2025-03-30 19:56:44 +1000

    remove waiter and add sleep

--- src/test/java/school/redrover/CreateNewFolderTest.java
+++ src/test/java/school/redrover/CreateNewFolderTest.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
 
@@ -12,7 +13,7 @@ import java.time.Duration;
 public class CreateNewFolderTest extends BaseTest {
 
     @Test
-    public void testCreateFolder() {
+    public void testCreateFolder() throws InterruptedException {
         getDriver().findElement(By.xpath("//a[contains(@it,'hudson')]")).click();
         getDriver().findElement(By.id("name")).sendKeys("NewFolder");
         getDriver().findElement(By.xpath("//li[contains(@class,'Folder')]")).click();
@@ -20,9 +21,8 @@ public class CreateNewFolderTest extends BaseTest {
         getDriver().findElement(By.xpath("//input[@checkdependson]")).sendKeys("Folder1");
         getDriver().findElement(By.name("Submit")).click();
 
-        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
-        String actualText = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//h1"))).getText();
+        Thread.sleep(2000);
 
-        Assert.assertEquals(actualText, "Folder1");
+        Assert.assertEquals(getDriver().findElement(By.xpath("//h1")).getText(), "Folder1");
     }
 }
