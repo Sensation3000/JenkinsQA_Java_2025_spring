ca1f58e48b3ea850dc49fda37e807741d68b63f5
dmkoro <armpeet5@gmail.com>
2025-03-30 16:01:28 -0700

    adds new domain and verifies it's been added.

---
+++ src/test/java/school/redrover/AddNewDomainTest.java
@@ -0,0 +1,69 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+import school.redrover.common.BaseTest;
+
+import java.time.Duration;
+import java.util.List;
+
+import static org.testng.Assert.*;
+
+
+public class AddNewDomainTest extends BaseTest {
+
+    private final SoftAssert softAssert = new SoftAssert() ;
+
+    @Test
+    public void testAddNewDomain(){
+        WebDriver driver = getDriver() ;
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
+
+        driver.findElement(By.xpath("//a[contains(.,'Manage Jenkins')]")).click();
+        driver.findElement(By.xpath("//a[@href='credentials']")).click();
+        driver.findElement(By.cssSelector("a[href='/manage/credentials/store/system']")).click();
+        //   WebElement addDomain = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@href='/manage/credentials/store/system/newDomain']")));
+        //   addDomain.click();
+        //    driver.findElement(By.xpath("//a[@href='newDomain']")).click();
+        //    System.out.println(driver.getCurrentUrl());
+        //    System.out.println("Current URL: " + driver.getCurrentUrl());
+
+//        List<WebElement> elements = driver.findElements(By.tagName("a"));
+//        for (WebElement element : elements) {
+//            System.out.println("Found link: " + element.getText() + " -> " + element.getAttribute("href"));
+//        }
+
+        driver.navigate().to("http://localhost:8080/manage/credentials/store/system");
+//
+//        JavascriptExecutor js = (JavascriptExecutor) driver;
+//        WebElement addDomain = driver.findElement(By.xpath("//a[@href='/manage/credentials/store/system/newDomain']"));
+//        js.executeScript("arguments[0].click();", addDomain);
+        driver.findElement(By.xpath("//a[@href='newDomain']")).click();
+
+        //    driver.findElement(By.xpath("//a[@href='/manage/credentials/store/system/newDomain']")).click();
+        //   System.out.println(driver.getCurrentUrl());
+        driver.findElement(By.xpath("//input[@name='_.name']")).sendKeys("Test Domain");
+        driver.findElement(By.xpath("//textarea[@name='description']")).sendKeys("Test domain description");
+        driver.findElement(By.xpath("//button[@type='submit' and @name='Submit']")).click();
+
+        String testDomainLink = driver.findElement(By.cssSelector("a[href='/manage/credentials/store/system/domain/Test%20Domain/']")).getText();
+        softAssert.assertEquals(testDomainLink, "Test Domain") ;
+
+        String testDomainDescription = driver.findElement(By.xpath("//div[text()='Test domain description']")).getText();
+        softAssert.assertEquals(testDomainDescription, "Test domain description") ;
+
+        String testDomainHeader = driver.findElement(By.xpath("//*[@id=\"main-panel\"]/div[1]/div[1]/h1")).getText();
+        softAssert.assertEquals(testDomainHeader, "Test Domain") ;
+
+        driver.findElement(By.xpath("//a[@href='delete']")).click();
+        driver.findElement(By.name("Submit")).click();
+        softAssert.assertAll();
+    }
+}
