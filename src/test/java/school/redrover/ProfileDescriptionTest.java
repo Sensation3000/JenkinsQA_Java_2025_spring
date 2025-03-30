d556461311c1d4163c2ac719c2b1549077e946d5
Olga-Genin <olga.genin@gmail.com>
2025-03-30 13:48:04 -0400

    add profile description

---
+++ src/test/java/school/redrover/ProfileDescriptionTest.java
@@ -0,0 +1,21 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.common.BaseTest;
+
+public class ProfileDescriptionTest extends BaseTest {
+
+    @Test
+    public void testAddDescription() throws InterruptedException {
+        WebDriver driver = getDriver();
+
+        driver.findElement(By.xpath("//a[@id='description-link']")).click();
+        driver.findElement(By.xpath("//textarea[@name='description']")).sendKeys("I'm a user");
+        driver.findElement(By.xpath("//button[@formnovalidate='formNoValidate']")).click();
+
+        Assert.assertTrue(driver.findElement(By.xpath("//div[@id='description']/div[1]")).getText().contains("I'm a user"));
+    }
+}
