9b0f9069ef4dd057c1e45d726fb18d62d5a78363
TolkachevaSE <71454954+TolkachevaSE@users.noreply.github.com>
2025-03-31 09:56:07 +0200

    createFolderTest added

---
+++ src/test/java/school/redrover/CreationItemsTest.java
@@ -0,0 +1,23 @@
+package school.redrover;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import school.redrover.common.BaseTest;
+
+public class CreationItemsTest extends BaseTest {
+    @Test
+    public void createFolderTest () throws InterruptedException {
+        WebDriver driver = getDriver();
+        String folderName = "NewFolder";
+        driver.findElement(By.linkText("Create a job")).click();
+        driver.findElement(By.cssSelector("[name='name']")).sendKeys(folderName);
+        driver.findElement(By.xpath("//span[text()='Folder']")).click();
+        driver.findElement(By.id("ok-button")).click();
+        driver.findElement(By.name("Submit")).click();
+        Thread.sleep(1000);
+        Assert.assertEquals(driver.findElement(By.xpath("//h1")).getText(),folderName,"Folder names should be the same");
+        Assert.assertEquals(driver.findElement(By.xpath("//h1/*")).getDomAttribute("tooltip"),"Folder","Names of tooltip should be the same");
+    }
+}
