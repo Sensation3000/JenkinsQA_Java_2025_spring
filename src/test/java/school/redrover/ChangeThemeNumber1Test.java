a17cecf6bfda536e861f3b4c7f47984aae0c15bd
ViktorSmiryagin <smiryaginviktorwtj@gmail.com>
2025-04-01 01:04:32 +0300

    success_created_item_check

--- src/test/java/school/redrover/ChangeThemeNumber1Test.java
+++ src/test/java/school/redrover/ChangeThemeNumber1Test.java
@@ -14,31 +14,28 @@ import java.util.List;
 public class ChangeThemeNumber1Test extends BaseTest {
 
     @Test
-    public void testOpenDashboardDropdown() throws InterruptedException {
-        Actions actions = new Actions(getDriver());
+    public void testOpenDashboardDropdown() {
+        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
+        WebElement dashboardLink = wait.until(ExpectedConditions.visibilityOfElementLocated(
+                By.xpath("//a[contains(text(),'Dashboard')]")
+        ));
+        new Actions(getDriver()).moveToElement(dashboardLink).perform();
+        List<WebElement> dropdowns = wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(
+                By.cssSelector("button.jenkins-menu-dropdown-chevron"), 1));
 
-        WebElement dashboardLink = getDriver().findElement(By.xpath("//a[contains(text(),'Dashboard')]"));
-
-        actions.moveToElement(dashboardLink).perform();
-        Actions actions1 = new Actions(getDriver());
-
-        WebElement dashboard = getDriver().findElement(By.xpath("//a[contains(text(), 'Dashboard')]"));
-        actions1.moveToElement(dashboard).perform();
-
-        List<WebElement> dropdowns = getDriver().findElements(By.cssSelector("button.jenkins-menu-dropdown-chevron"));
-        dropdowns.get(1).click();
-
-        WebElement manageJenkins = getDriver().findElement(By.cssSelector("a.jenkins-dropdown__item[href='/manage']"));
-
-        Actions actionss = new Actions(getDriver());
-        actionss.moveToElement(manageJenkins).perform();
-
-        WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(5));
-        WebElement appearance = wait.until(ExpectedConditions.visibilityOfElementLocated(
+        WebElement dropdownButton = wait.until(ExpectedConditions.elementToBeClickable(dropdowns.get(1)));
+        dropdownButton.click();
+        WebElement manageJenkins = wait.until(ExpectedConditions.elementToBeClickable(
+                By.cssSelector("a.jenkins-dropdown__item[href='/manage']")
+        ));
+        new Actions(getDriver()).moveToElement(manageJenkins).perform();
+        WebElement appearance = wait.until(ExpectedConditions.elementToBeClickable(
                 By.cssSelector("a.jenkins-dropdown__item[href='/manage/appearance']")
         ));
         appearance.click();
-        List<WebElement> themes = getDriver().findElements(By.cssSelector("input[type='radio'][name$='.theme']"));
+        List<WebElement> themes = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(
+                By.cssSelector("input[type='radio'][name$='.theme']")
+        ));
 
         int selectedIndex = -1;
         for (int i = 0; i < themes.size(); i++) {
@@ -47,20 +44,17 @@ public class ChangeThemeNumber1Test extends BaseTest {
                 break;
             }
         }
-
         int nextIndex = (selectedIndex + 1) % themes.size();
         String nextId = themes.get(nextIndex).getAttribute("id");
 
         WebElement label = getDriver().findElement(By.cssSelector("label[for='" + nextId + "']"));
         label.click();
-
-        WebElement saveButton = getDriver().findElement(By.cssSelector("button[name='Submit']"));
+        WebElement saveButton = wait.until(ExpectedConditions.elementToBeClickable(
+                By.cssSelector("button[name='Submit']")
+        ));
         saveButton.click();
-
-        WebDriverWait wait1 = new WebDriverWait(getDriver(), Duration.ofSeconds(5));
-        WebElement searchInput = wait1.until(ExpectedConditions.visibilityOfElementLocated(
+        wait.until(ExpectedConditions.visibilityOfElementLocated(
                 By.cssSelector("input#settings-search-bar")
         ));
-
     }
 }
