e96029e120f7eb64f3834346475508c6e8e2f1c0
NatKa-181 <niki.in.chikago@gmail.com>
2025-03-31 13:14:38 +0600

    Add JS

--- src/test/java/school/redrover/JenkinsTest.java
+++ src/test/java/school/redrover/JenkinsTest.java
@@ -1,34 +1,25 @@
 package school.redrover;
-
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
 
-import java.time.Duration;
-
-import static org.testng.AssertJUnit.assertNotNull;
-import static org.testng.AssertJUnit.assertTrue;
 
 
     public class JenkinsTest extends BaseTest {
+
         @Test
-        public void TestCreateNewPipeline()  {
-            WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(1));
-            getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
-            getDriver().findElement(
-                    By.xpath("//*[@id='name']")).sendKeys("Test1");
-            getDriver().findElement(
-                    By.xpath("//*[@class='org_jenkinsci_plugins_workflow_job_WorkflowJob']")).click();
-            getDriver().findElement(By.id("ok-button")).click();
-            getDriver().findElement(
-                    By.xpath("//*[@name='description']")).sendKeys("First Test");
-            getDriver().findElement(By.cssSelector("a[href='/']")).click();
-            WebElement addDescription = wait.until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='description-link']")));
-            assertNotNull(addDescription);
-            assertTrue(addDescription.isDisplayed());
+        public void TestCreateNewPipeline() {
+            JavascriptExecutor js = (JavascriptExecutor) getDriver();
+            WebElement newJobLink = getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']"));
+            js.executeScript("arguments[0].click();", newJobLink);
+            WebElement nameField = getDriver().findElement(By.xpath("//*[@id='name']"));
+            js.executeScript("arguments[0].value = 'Test1';", nameField);
+            WebElement workflowJob = getDriver().findElement(By.xpath("//*[@class='org_jenkinsci_plugins_workflow_job_WorkflowJob']"));
+            js.executeScript("arguments[0].click();", workflowJob);
+            WebElement okButton = getDriver().findElement(By.id("ok-button"));
+            js.executeScript("arguments[0].click();", okButton);
         }
     }
 
