0ea6f1515e4be8eae602fabca19f62284ff7b6f2
ViktorSmiryagin <smiryaginviktorwtj@gmail.com>
2025-04-01 02:03:11 +0300

    success_created_item_check

--- src/test/java/school/redrover/ChangeThemeNumber1Test.java
+++ src/test/java/school/redrover/ChangeThemeNumber1Test.java
@@ -3,7 +3,6 @@ package school.redrover;
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.Test;
@@ -13,31 +12,43 @@ import java.time.Duration;
 import java.util.List;
 
 public class ChangeThemeNumber1Test extends BaseTest {
+    private WebDriverWait wait5;
+
+    protected WebDriverWait getWait5() {
+        if (wait5 == null) {
+            wait5 = new WebDriverWait(getDriver(), Duration.ofSeconds(5));
+        }
+        return wait5;
+    }
 
     @Test
-    public void testOpenDashboardDropdown() {
+    public void testOpenDashboardDropdown() throws InterruptedException {
+        JavascriptExecutor js = (JavascriptExecutor) getDriver();
         WebDriverWait wait = new WebDriverWait(getDriver(), Duration.ofSeconds(10));
-        WebElement dashboardLink = wait.until(ExpectedConditions.visibilityOfElementLocated(
-                By.xpath("//a[contains(text(),'Dashboard')]")
-        ));
-        new Actions(getDriver()).moveToElement(dashboardLink).perform();
-        List<WebElement> dropdowns = wait.until(ExpectedConditions.numberOfElementsToBeMoreThan(
-                By.cssSelector("button.jenkins-menu-dropdown-chevron"), 1));
-        WebElement dropdownButton = dropdowns.get(1);
-        ((JavascriptExecutor) getDriver()).executeScript("arguments[0].scrollIntoView(true);", dropdownButton);
-        ((JavascriptExecutor) getDriver()).executeScript("arguments[0].click();", dropdownButton);
-        WebElement manageJenkins = wait.until(ExpectedConditions.elementToBeClickable(
-                By.cssSelector("a.jenkins-dropdown__item[href='/manage']")
-        ));
-        new Actions(getDriver()).moveToElement(manageJenkins).perform();
-        WebElement appearance = wait.until(ExpectedConditions.elementToBeClickable(
-                By.cssSelector("a.jenkins-dropdown__item[href='/manage/appearance']")
-        ));
-        appearance.click();
-        List<WebElement> themes = wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(
-                By.cssSelector("input[type='radio'][name$='.theme']")
-        ));
 
+        js.executeScript("arguments[0].dispatchEvent(new Event('mouseenter'));",
+                getWait5().until(ExpectedConditions.visibilityOfElementLocated(
+                        By.xpath("//li[@class='jenkins-breadcrumbs__list-item']"))));
+        js.executeScript("arguments[0].dispatchEvent(new Event('mouseenter'));",
+                getWait5().until(ExpectedConditions.visibilityOfElementLocated(
+                        By.xpath("//button[@class='jenkins-menu-dropdown-chevron' and @data-href='http://localhost:8080/']"))));
+        js.executeScript("arguments[0].click();",
+                getWait5().until(ExpectedConditions.visibilityOfElementLocated(
+                        By.xpath("//button[@class='jenkins-menu-dropdown-chevron' and @data-href='http://localhost:8080/']"))));
+        Thread.sleep(1000);
+
+        js.executeScript("arguments[0].dispatchEvent(new Event('mouseenter'));",
+                getWait5().until(ExpectedConditions.visibilityOfElementLocated(
+                        By.xpath("//a[contains(@href,'manage') and contains(@class,'dropdown')]"))));
+        js.executeScript("arguments[0].dispatchEvent(new Event('mouseenter'));",
+                getWait5().until(ExpectedConditions.visibilityOfElementLocated(
+                        By.xpath("//a[@href='/manage/appearance']"))));
+        js.executeScript("arguments[0].click();",
+                getWait5().until(ExpectedConditions.visibilityOfElementLocated(
+                        By.xpath("//a[@href='/manage/appearance']"))));
+        Thread.sleep(1000);
+
+        List<WebElement> themes = getDriver().findElements(By.cssSelector("input[type='radio'][name$='.theme']"));
         int selectedIndex = -1;
         for (int i = 0; i < themes.size(); i++) {
             if (themes.get(i).isSelected()) {
@@ -49,12 +60,8 @@ public class ChangeThemeNumber1Test extends BaseTest {
         String nextId = themes.get(nextIndex).getAttribute("id");
         WebElement label = getDriver().findElement(By.cssSelector("label[for='" + nextId + "']"));
         label.click();
-        WebElement saveButton = wait.until(ExpectedConditions.elementToBeClickable(
-                By.cssSelector("button[name='Submit']")
-        ));
+        WebElement saveButton = getDriver().findElement(By.cssSelector("button[name='Submit']"));
         saveButton.click();
-        wait.until(ExpectedConditions.visibilityOfElementLocated(
-                By.cssSelector("input#settings-search-bar")
-        ));
+        getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector("input#settings-search-bar")));
     }
 }
