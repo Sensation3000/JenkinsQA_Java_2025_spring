2b06c2f063c874c8c4c281b16bf30eee461b2155
NataliaMiro <natalymir82@gmail.com>
2025-04-01 09:09:56 +0300

    Добавила тест на проверку кнопки Description и тест на сохранение текста в поле Description

---
+++ src/test/java/school/redrover/common/StudentUnionTest.java
@@ -0,0 +1,50 @@
+package school.redrover.common;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import static org.testng.AssertJUnit.assertTrue;
+
+
+
+public class StudentUnionTest extends BaseTest{
+
+
+
+    @Test
+    public void openDescriptionForm(){
+
+       WebDriver driver = getDriver();
+
+        // Поиск кнопки по ID
+        WebElement descriptionLink = driver.findElement(By.xpath("//*[@id=\"description-link\"]"));
+
+        // Проверка, что кнопка отображается
+        assertTrue("Кнопка description-link не отображается", descriptionLink.isDisplayed());
+        //System.out.println("Кнопка description-link отображается");
+
+        // Проверка, что кнопка кликабельна
+        assertTrue("Кнопка description-link не кликабельна", descriptionLink.isEnabled());
+        //System.out.println("Кнопка description-link кликабельна");
+
+        // Клик по кнопке
+        descriptionLink.click();
+
+    }
+
+    @Test
+    public void addDescription(){
+        // Ввод текстового описания в поле описание
+        String textDescription = "Text description";
+        getDriver().findElement(By.xpath("//a[text()='Add description']")).click();
+        getDriver().findElement(By.name("description")).sendKeys(textDescription);
+        // Сохранение текста
+        getDriver().findElement(By.name("Submit")).click();
+
+        Assert.assertTrue(getDriver().findElement(By.xpath("//div[text()='" + textDescription + "']")).isDisplayed());
+
+    }
+}
