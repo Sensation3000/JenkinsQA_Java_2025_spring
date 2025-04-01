f0bf86211fb92f319619c592c6086359e1c150c3
NataliaMiro <natalymir82@gmail.com>
2025-04-01 11:04:21 +0300

    Перенесла тесты в папку school.redrover

--- src/test/java/school/redrover/StudentUnionTest.java
+++ src/test/java/school/redrover/StudentUnionTest.java
@@ -1,16 +1,17 @@
-package school.redrover.common;
+package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import school.redrover.common.BaseTest;
 
 import static org.testng.AssertJUnit.assertTrue;
 
 
 
-public class StudentUnionTest extends BaseTest{
+public class StudentUnionTest extends BaseTest {
 
 
 
