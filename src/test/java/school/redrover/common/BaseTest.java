77e3940769e7413dd5b1b4c8424578cae714763b
AnnaLimutina <zip1061@mail.ru .>
2025-04-01 22:34:16 -0500

    add screenshots

--- src/test/java/school/redrover/common/BaseTest.java
+++ src/test/java/school/redrover/common/BaseTest.java
@@ -1,9 +1,14 @@
 package school.redrover.common;
 
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.testng.ITestResult;
 import org.testng.annotations.*;
 
+import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.Method;
 
 public abstract class BaseTest {
@@ -26,6 +31,23 @@ public abstract class BaseTest {
         if (ProjectUtils.isRunCI() || testResult.isSuccess() || ProjectUtils.closeIfError()) {
             JenkinsUtils.logout(driver);
             driver.quit();
+        }
+
+        if (!testResult.isSuccess()) {
+            try {
+                File screenshotDir = new File("screenshots");
+                if (!screenshotDir.exists()) {
+                    screenshotDir.mkdirs();
+                }
+
+                File screenshot = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
+                String fileName = testResult.getTestName() + ".png";
+                File destination = new File(screenshotDir, fileName);
+                FileUtils.copyFile(screenshot, destination);
+
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
         }
 
         ProjectUtils.logf("Execution time is %.3f sec", (testResult.getEndMillis() - testResult.getStartMillis()) / 1000.0);
