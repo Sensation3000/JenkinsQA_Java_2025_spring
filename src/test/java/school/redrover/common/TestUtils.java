161902edd90c6b0d392f101e6009bc0c792970f5
IvanSoregashi <ivanshiriaev2222@gmail.com>
2025-04-04 00:22:40 +0500

    Looks like it was a fluke, restored gotoHomePage to original condition

--- src/test/java/school/redrover/common/TestUtils.java
+++ src/test/java/school/redrover/common/TestUtils.java
@@ -1,15 +1,13 @@
 package school.redrover.common;
 
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class TestUtils {
 
     public static void gotoHomePage(BaseTest baseTest) {
-        WebDriverWait wait10 = baseTest.getWait10();
-        wait10.until(ExpectedConditions.presenceOfElementLocated(By.id("jenkins-home-link")));
-        wait10.until(ExpectedConditions.elementToBeClickable(By.id("jenkins-home-link"))).click();
+        baseTest.getWait10()
+                .until(ExpectedConditions.elementToBeClickable(By.id("jenkins-home-link")))
+                .click();
     }
 }
