4f10a9760179212eae6413fb5097c4b799c36d85
IvanSoregashi <ivanshiriaev2222@gmail.com>
2025-04-03 07:32:40 +0500

    sped up my test by 50 times

--- src/test/java/school/redrover/EsotericTest.java
+++ src/test/java/school/redrover/EsotericTest.java
@@ -29,19 +29,9 @@ public class EsotericTest extends BaseTest {
 
         WebDriver driver = getDriver();
         Actions builder = new Actions(driver);
-        WebDriverWait wait30 = new WebDriverWait(driver, Duration.ofSeconds(30));
 
-        WebElement dashboard_crumb = driver.findElement(By.cssSelector("div#breadcrumbBar a.model-link"));
-        WebElement dashboard_chevron = dashboard_crumb.findElement(By.cssSelector("button.jenkins-menu-dropdown-chevron"));
-
-        builder.moveToElement(dashboard_crumb).moveToElement(dashboard_chevron).click().perform();
-        wait30.until(ExpectedConditions.invisibilityOfElementLocated(By.cssSelector("p.jenkins-spinner")));
-
-        WebElement manageJenkins = driver.findElement(By.cssSelector("a.jenkins-dropdown__item span.jenkins-dropdown__item__chevron"));
-        builder.moveToElement(manageJenkins).perform();
-
-        WebElement aboutJenkins = driver.findElement(By.cssSelector("a[href='/manage/about']"));
-        builder.moveToElement(aboutJenkins).click().perform();
+        driver.findElement(By.cssSelector("button.jenkins_ver")).click();
+        builder.moveToElement(driver.findElement(By.cssSelector("a[href='/manage/about']"))).click().perform();
 
         WebElement jenkinsVersion = driver.findElement(By.cssSelector("p.app-about-version"));
         Assert.assertEquals(jenkinsVersion.getText(), "Version 2.492.2");
