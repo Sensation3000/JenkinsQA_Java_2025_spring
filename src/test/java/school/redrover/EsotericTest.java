0cd697925ea0ac742ca4e0f3179372aed42bd9d7
IvanSoregashi <ivanshiriaev2222@gmail.com>
2025-04-03 07:40:07 +0500

    cleanup imports

--- src/test/java/school/redrover/EsotericTest.java
+++ src/test/java/school/redrover/EsotericTest.java
@@ -4,13 +4,9 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
-
-import java.time.Duration;
 
 public class EsotericTest extends BaseTest {
 
