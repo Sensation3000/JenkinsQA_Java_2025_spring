cd18d0cb1f1b914b069165ba9a4f591e99268456
Petrichor <180253417+IrinaPetrichor@users.noreply.github.com>
2025-04-03 19:33:23 -0400

    AT_24.001.01 | Sign in/out > Sign in > Signing in with invalid credentials shows an error (#617)
    * Add testInvalidCredentialsError

--- src/test/java/school/redrover/common/TestUtils.java
+++ src/test/java/school/redrover/common/TestUtils.java
@@ -1,7 +1,10 @@
 package school.redrover.common;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+
+import java.util.UUID;
 
 public class TestUtils {
 
@@ -9,5 +12,16 @@ public class TestUtils {
         baseTest.getWait10()
                 .until(ExpectedConditions.elementToBeClickable(By.id("jenkins-home-link")))
                 .click();
+    }
+
+    public static WebElement waitForHomePageLoad(BaseTest baseTest) {
+        return baseTest
+                .getWait5()
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[@href='/logout']")));
+    }
+
+    public static String generateRandomAlphanumeric() {
+
+        return UUID.randomUUID().toString().replaceAll("-", "");
     }
 }
