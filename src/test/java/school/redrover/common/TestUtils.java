d29c655cc9eacf588afc98a8b6f2c6334daefa5f
IvanSoregashi <ivanshiriaev2222@gmail.com>
2025-04-03 23:53:05 +0500

    refactored gotoHomePage, lets see if it works

--- src/test/java/school/redrover/common/TestUtils.java
+++ src/test/java/school/redrover/common/TestUtils.java
@@ -1,13 +1,16 @@
 package school.redrover.common;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class TestUtils {
 
     public static void gotoHomePage(BaseTest baseTest) {
-        baseTest.getWait10()
-                .until(ExpectedConditions.elementToBeClickable(By.id("jenkins-home-link")))
-                .click();
+        WebDriverWait wait10 = baseTest.getWait10();
+        wait10.until(ExpectedConditions.presenceOfElementLocated(By.id("jenkins-home-link")));
+        WebElement homeLink = wait10.until(ExpectedConditions.elementToBeClickable(By.id("jenkins-home-link")));
+        homeLink.click();
     }
 }
