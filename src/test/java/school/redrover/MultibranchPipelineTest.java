37dc58f1ee431d95953e299c2b8ee7c44fe3b183
VyacheslavBeliaev <vbelyaev86@gmail.com>
2025-04-06 22:39:21 +0300

    update test create with exist name

--- src/test/java/school/redrover/MultibranchPipelineTest.java
+++ src/test/java/school/redrover/MultibranchPipelineTest.java
@@ -2,10 +2,14 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Wait;
 import org.testng.Assert;
 import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
+
 
 public class MultibranchPipelineTest extends BaseTest {
 
@@ -35,8 +39,8 @@ public class MultibranchPipelineTest extends BaseTest {
         final String projectName = "MultiBuild";
         final String errorMessage = "» A job already exists with the name " + "‘" + projectName + "’";
 
-        getDriver().findElement(By.cssSelector("[href$='/newJob']")).click();
-        getDriver().findElement(By.id("name")).sendKeys(projectName);
+        getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='tasks']/div[1]/span/a"))).click();
+        getDriver().findElement(By.className("jenkins-input")).sendKeys(projectName);
 
         JavascriptExecutor js = (JavascriptExecutor) getDriver();
         js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
@@ -48,15 +52,15 @@ public class MultibranchPipelineTest extends BaseTest {
         js1.executeScript("window.scrollTo(0, document.body.scrollHeight);");
 
         getDriver().findElement(By.name("Submit")).click();
-        Thread.sleep(2000);
-        getDriver().findElement(By.xpath("//*[@id='jenkins-name-icon']")).click();
+        getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='jenkins-name-icon']"))).click();
 
-        getDriver().findElement(By.cssSelector("[href$='/newJob']")).click();
+        getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//*[@id='tasks']/div[1]/span/a"))).click();
+
         JavascriptExecutor js2 = (JavascriptExecutor) getDriver();
         js2.executeScript("window.scrollTo(0, document.body.scrollHeight);");
 
-        getDriver().findElement(By.cssSelector("[class$='MultiBranchProject']")).click();
         getDriver().findElement(By.id("name")).sendKeys(projectName);
+        getDriver().findElement(By.cssSelector("[class$='MultiBranchProject']")).click();
 
         String actualMessage = getDriver().findElement(By.xpath("//*[@id='itemname-invalid']")).getText();
         Assert.assertEquals(actualMessage, errorMessage);
