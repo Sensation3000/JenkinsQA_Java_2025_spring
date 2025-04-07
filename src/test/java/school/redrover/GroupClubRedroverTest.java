9c41c02adff5fdc7875387091359c51c56ac8d6f
kuletsky <kuletsky@gmail.com>
2025-04-07 12:27:29 -0400

    commented add waits

--- src/test/java/school/redrover/GroupClubRedroverTest.java
+++ src/test/java/school/redrover/GroupClubRedroverTest.java
@@ -3,7 +3,6 @@ package school.redrover;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import org.testng.asserts.SoftAssert;
 import school.redrover.common.BaseTest;
