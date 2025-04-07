5f2f531af08816416f4ebcffb20e2e7f338ff309
Ekaterina Zarubina <zarubina.e4@wb.ru>
2025-04-07 12:33:52 +0700

    fix: unused imports deleted ez/foldertest

--- src/test/java/school/redrover/Folder2Test.java
+++ src/test/java/school/redrover/Folder2Test.java
@@ -2,15 +2,10 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
-
-import java.util.List;
 import java.util.UUID;
-
-import static java.lang.Thread.sleep;
 
 public class Folder2Test extends BaseTest {
 
