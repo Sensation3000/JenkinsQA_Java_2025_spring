c5052338f9fa15e6e5a37ef7144ed71a2797ff65
Marina <164689579+Marinam026@users.noreply.github.com>
2025-04-07 20:38:13 -0400

    test for message

--- src/test/java/school/redrover/CreateNewJobTest.java
+++ src/test/java/school/redrover/CreateNewJobTest.java
@@ -9,13 +9,10 @@ public class CreateNewJobTest extends BaseTest {
 
     @Test
     public void testNewJob() {
-        getDriver().findElement(By.xpath(
-                "//*[@id=\"main-panel\"]/div[2]/div/section[1]/ul/li/a/span[1]")).click();
-        getDriver().findElement(By.xpath(
-                "//*[@id=\"j-add-item-type-standalone-projects\"]/ul/li[1]/div[2]/div")).click();
+        getDriver().findElement(By.className("content-block__link")).click();
+        getDriver().findElement(By.className("label")).click();
         String message = "» This field cannot be empty, please enter a valid name";
-        Assert.assertEquals(getDriver().findElement(By.xpath(
-                "//*[@id=\"itemname-required\"]")).getText(), message);
+        Assert.assertEquals(getDriver().findElement(By.id("itemname-required")).getText(), message);
     }
 
 }
