21f3ad0a14c7910fba304bdcde1a9b7bfda33a61
Anastasia Bodunova <anprado21@gmail.com>
2025-04-09 00:35:54 +0200

    BuildJob and Delete Build

--- src/test/java/school/redrover/BuildJobTest.java
+++ src/test/java/school/redrover/BuildJobTest.java
@@ -18,7 +18,8 @@ public class BuildJobTest extends BaseTest {
         getDriver().findElement(By.className("hudson_model_FreeStyleProject")).click();
         getDriver().findElement(By.id("ok-button")).click();
         getDriver().findElement(By.name("Submit")).sendKeys(Keys.ENTER);
-        TestUtils.gotoHomePage(this);
+        getWait10().until(ExpectedConditions.elementToBeClickable(
+                By.className("jenkins-breadcrumbs__list-item"))).click();
 
         By jobButton = By.xpath("//td/a/span[text() = '%s']/../button".formatted(jobName));
         WebElement button = getWait5().until(ExpectedConditions.elementToBeClickable(jobButton));
@@ -43,7 +44,6 @@ public class BuildJobTest extends BaseTest {
         getDriver().findElement(By.xpath("//a[@href='/job/Test%20item/lastBuild/confirmDelete']")).click();
         getDriver().findElement(By.name("Submit")).click();
         getDriver().findElement(By.xpath("//a[@href='/job/Test%20item/changes']")).click();
-
         String infoBuilds = getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.id("main-panel"))).getText();
 
         assertTrue("Build was not deleted successfully", infoBuilds.contains("No builds"));
