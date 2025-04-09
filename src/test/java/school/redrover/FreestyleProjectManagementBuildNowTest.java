d74486b2c8ddbf21efbe75e3175d37cbcc4b477d
Anna_Tut <112267571+Tyutyakova@users.noreply.github.com>
2025-04-09 14:17:26 +0300

    Update FreestyleProjectManagementBuildNowTest.java

--- src/test/java/school/redrover/FreestyleProjectManagementBuildNowTest.java
+++ src/test/java/school/redrover/FreestyleProjectManagementBuildNowTest.java
@@ -22,13 +22,18 @@ public class FreestyleProjectManagementBuildNowTest extends BaseTest {
         driver.findElement(By.id("ok-button")).click();
         driver.findElement(By.name("Submit")).click();
     }
+    
     @Test
     public void testAvailableBuildNowOnProjectPage() {
         WebDriver driver = getDriver();
+        
         createNewItemFrestyle(driver);
+        
         TestUtils.gotoHomePage(this);
+        
         driver.findElement(By.xpath("//span[text()='" + name_Freestyle_Project + "']")).click();
         driver.findElement(By.xpath(  "//*[@class='task-link task-link-no-confirm ' and contains(@href,'build')]")).click();
+       
         Assert.assertEquals(
                 getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.id("notification-bar"))).getText(),
                 "Build scheduled");
