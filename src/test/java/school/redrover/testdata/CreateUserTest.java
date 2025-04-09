cf67d8a00e71aa30f100a64fa228a2c30c645384
Mikhail Labkovskiy <artas91@mail.ru>
2025-04-09 23:44:30 +0300

    Merge pull request #826 from RedRoverSchool/ob/disabled-pipeline
    Disabled Pipeline

---
+++ src/test/java/school/redrover/testdata/CreateUserTest.java
@@ -0,0 +1,15 @@
+package school.redrover.testdata;
+
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+import school.redrover.common.BaseTest;
+
+public class CreateUserTest  extends BaseTest {
+
+    @Test
+    public  void testCreateUser() {
+       WebDriver driver  = getDriver() ;
+
+    }
+
+}
