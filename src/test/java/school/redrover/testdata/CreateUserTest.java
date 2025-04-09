b678016077c2c0f8d1180a511dd3c522612d2cf6
Mikhail Labkovskiy <artas91@mail.ru>
2025-04-10 00:19:32 +0300

    Merge pull request #826 from RedRoverSchool/ob/disabled-pipeline
    Disabled Pipeline

--- src/test/java/school/redrover/testdata/CreateUserTest.java
+++ src/test/java/school/redrover/testdata/CreateUserTest.java
@@ -1,15 +1,28 @@
 package school.redrover.testdata;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
+
+import java.time.Duration;
 
 public class CreateUserTest  extends BaseTest {
 
     @Test
-    public  void testCreateUser() {
+    public  void testCreateUser() throws InterruptedException {
        WebDriver driver  = getDriver() ;
+       driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(2));
+       WebElement manageButton = driver.findElement(By.xpath("//a[@href='/manage']"));
+       Assert.assertEquals(manageButton.getText(),"Manage Jenkins");
+       Thread.sleep(1000);
+
+
+
 
     }
+
 
 }
