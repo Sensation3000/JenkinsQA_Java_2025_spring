1f6cb4f7eb08e8808f650d6002401c5c3a9b6259
jlNullPointerException <victoriavesp@mail.ru>
2025-04-09 18:03:35 +0300

    TC_01.001.21 | New Item > Creatе a new item > Error checking for Freestyle Project. Additional cases

--- src/test/java/school/redrover/CheckErrorsForNewItemTest.java
+++ src/test/java/school/redrover/CheckErrorsForNewItemTest.java
@@ -2,8 +2,6 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
