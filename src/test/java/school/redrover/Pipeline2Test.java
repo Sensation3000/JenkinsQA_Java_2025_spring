5727267d46d1c88f92bd0baf6700e0109a660abd
viktoriak12 <viktoria.k12@mail.ru>
2025-04-09 17:00:59 +0300

    Add Jenkins TC

--- src/test/java/school/redrover/Pipeline2Test.java
+++ src/test/java/school/redrover/Pipeline2Test.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
+import school.redrover.common.TestUtils;
 
 public class Pipeline2Test extends BaseTest {
 
@@ -21,5 +22,23 @@ public class Pipeline2Test extends BaseTest {
         driver.findElement(By.name("Submit")).click();
 
         Assert.assertEquals(driver.findElement(By.className("job-index-headline")).getText(), projectName);
+    }
+
+    @Test
+    public void testCopyFromError() {
+        final String freestyleProjectA = "FirstProject";
+        final String freestyleProjectB = "SecondProject";
+        final String copyFrom = "No such item";
+
+        WebDriver driver = getDriver();
+        TestUtils.newItemCreate(this, freestyleProjectA, 2);
+
+        driver.findElement(By.xpath("//*[@id=\"tasks\"]/div[1]/span/a")).click();
+        driver.findElement(By.className("jenkins-input")).sendKeys(freestyleProjectB);
+        driver.findElement(By.id("from")).sendKeys(copyFrom);
+        driver.findElement(By.id("ok-button")).click();
+
+        Assert.assertEquals(driver.findElement(By.xpath("//*[@id=\"main-panel\"]/h1")).getText(),"Error");
+        Assert.assertEquals(driver.findElement(By.xpath("//*[@id=\"main-panel\"]/p")).getText(), "No such job: " + copyFrom);
     }
 }
