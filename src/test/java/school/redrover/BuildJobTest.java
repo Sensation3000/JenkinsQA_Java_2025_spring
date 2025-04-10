dffe5c22bfc14ddb4e7cad51bcee1f676a98befb
Anastasia Bodunova <anprado21@gmail.com>
2025-04-10 15:07:45 +0200

    Build Job and Delete job test refactoring

--- src/test/java/school/redrover/BuildJobTest.java
+++ src/test/java/school/redrover/BuildJobTest.java
@@ -1,24 +1,19 @@
 package school.redrover;
 import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
 import school.redrover.common.TestUtils;
 import static org.testng.AssertJUnit.assertTrue;
 
 public class BuildJobTest extends BaseTest {
-    @Ignore //BuildJobTest.testBuildJob:20 » StaleElementReference stale element reference: stale element not found
+    private static final String jobName = "Test item";
+
     @Test
     public void testBuildJob() {
-        final String jobName = "Test item";
-
-        getDriver().findElement(By.xpath("//a[@href='/view/all/newJob']")).click();
-        getDriver().findElement(By.id("name")).sendKeys(jobName);
-        getDriver().findElement(By.className("hudson_model_FreeStyleProject")).click();
-        getDriver().findElement(By.id("ok-button")).click();
-        getDriver().findElement(By.name("Submit")).click();
-        getWait10().until(ExpectedConditions.elementToBeClickable(By.xpath("//a[@href='/']"))).click();
+        TestUtils.createFreestyleProject(getDriver(), jobName);
+        getDriver().findElement(By.xpath("//*[@id='tasks']/div[2]/span/button")).click();
+        TestUtils.gotoHomePage(this);
 
         By jobButton = By.xpath("//td/a/span[text() = '%s']/../button".formatted(jobName));
         WebElement button = getWait5().until(ExpectedConditions.elementToBeClickable(jobButton));
@@ -39,14 +34,16 @@ public class BuildJobTest extends BaseTest {
 
         assertTrue("В Console Output отсутствует запись об успешной сборке", actualText.contains(expectedText));
     }
-    @Ignore
+
     @Test
     public void testDeleteBuild () {
         testBuildJob();
-        getDriver().findElement(By.xpath("//a[@href='/job/Test%20item/lastBuild/confirmDelete']")).click();
+        getDriver().findElement(
+                By.xpath("//a[@href='/job/Test%20item/lastBuild/confirmDelete']")).click();
         getDriver().findElement(By.name("Submit")).click();
         getDriver().findElement(By.xpath("//a[@href='/job/Test%20item/changes']")).click();
-        String infoBuilds = getWait5().until(ExpectedConditions.visibilityOfElementLocated(By.id("main-panel"))).getText();
+        String infoBuilds = getWait5().
+                until(ExpectedConditions.visibilityOfElementLocated(By.id("main-panel"))).getText();
 
         assertTrue("Build was not deleted successfully", infoBuilds.contains("No builds"));
     }
