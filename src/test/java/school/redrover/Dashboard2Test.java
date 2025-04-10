941651afe85c1adadb2d5c82c8e0ac60528ea775
Margarita Shlykova <margarita.shlykova@medm.com>
2025-04-09 23:45:09 -0400

    TC_11.002.01 | Welcome Dashboard > Create a job > Check it is possible to create job from Dashboard

--- src/test/java/school/redrover/Dashboard2Test.java
+++ src/test/java/school/redrover/Dashboard2Test.java
@@ -1,6 +1,7 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
@@ -11,6 +12,9 @@ public class Dashboard2Test extends BaseTest {
     public void testPossibleToCreateJobFromDashboard() {
         getDriver().findElement(By.xpath("//li/a[@href='newJob']")).click();
 
-        Assert.assertTrue(getDriver().findElement(By.id("add-item-panel")).isDisplayed());
+        Assert.assertTrue(
+                getWait5().until(ExpectedConditions.visibilityOf(
+                        getDriver().findElement(By.id("add-item-panel")))).isDisplayed());
+
     }
 }
