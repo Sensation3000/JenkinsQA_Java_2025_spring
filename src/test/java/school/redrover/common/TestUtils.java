dc0e2593e347b70a89db84f35fd0c6221dfb3ca4
DmKlv <dmklvd@gmail.com>
2025-04-10 20:06:44 +0200

    testNewItemCreation

--- src/test/java/school/redrover/common/TestUtils.java
+++ src/test/java/school/redrover/common/TestUtils.java
@@ -174,10 +174,12 @@ public class TestUtils {
     public static void clickJenkinsHomeLink(WebDriver driver) {
         WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
         try {
+
             WebElement homeLink = wait.until(ExpectedConditions.elementToBeClickable(By.id("jenkins-home-link")));
             ProjectUtils.log("Элемент 'jenkins-home-link' найден.");
 
             if (homeLink != null && homeLink.isDisplayed() && homeLink.isEnabled()) {
+
                 JavascriptExecutor js = (JavascriptExecutor) driver;
                 js.executeScript("arguments[0].click();", homeLink);
                 ProjectUtils.log("Клик по 'jenkins-home-link' выполнен успешно.");
@@ -186,10 +188,14 @@ public class TestUtils {
             }
         } catch (TimeoutException e) {
             ProjectUtils.log("Время ожидания для элемента 'jenkins-home-link' истекло.");
+        } catch (StaleElementReferenceException e) {
+            ProjectUtils.log("Элемент 'jenkins-home-link' устарел (Stale Element Reference).");
+        } catch (NoSuchElementException e) {
+            ProjectUtils.log("Элемент 'jenkins-home-link' не найден на странице.");
         } catch (Exception e) {
             ProjectUtils.log("Произошла ошибка при клике на 'jenkins-home-link': " + e.getMessage());
         }
-    } // JavascriptExecutor необходим, т.к. метод gotoHomePage работает не стабильно
+    }
 
     public static void createProjectWithName(WebDriver driver, String projectName, int projectTypeId) {
         driver.findElement(By.linkText("New Item")).click();
