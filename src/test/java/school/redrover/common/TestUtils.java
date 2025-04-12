3baea220695e47a63a69f4659afd6b7af50d1b04
rapppersha <rapppersha@yandex.ru>
2025-04-12 12:42:35 -0700

    clean code in TestUtils

--- src/test/java/school/redrover/common/TestUtils.java
+++ src/test/java/school/redrover/common/TestUtils.java
@@ -4,9 +4,7 @@ import org.openqa.selenium.*;
 
 import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.time.Duration;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.UUID;
