3720893c741f00056553ffb2b02a82fdffdd8a2f
Olga Senina <92520405+Ellutien@users.noreply.github.com>
2025-04-15 10:06:01 +0300

    Разделила тесты согласно правилу "один тест - один ПР", внесу отдельными ПР по очереди. Тест testButtonsNames

--- src/test/java/school/redrover/DisplaySystemInfoTest.java
+++ src/test/java/school/redrover/DisplaySystemInfoTest.java
@@ -22,54 +22,5 @@ public class DisplaySystemInfoTest extends BaseTest {
         Assert.assertEquals(manageJ, "Manage Jenkins");
         Assert.assertEquals(systemSet, "System Information");
     }
-
-    @Test
-    public void testShowSystemProperties() {
-        WebDriver driver = getDriver();
-
-        driver.findElement(By.xpath("//*[@id=\"tasks\"]/div[3]")).click();
-        driver.findElement(By.xpath("//*[@id=\"main-panel\"]/section[4]/div/div[1]")).click();
-        driver.findElement(By.xpath("//*[@id=\"main-panel\"]/div[3]/div/button[1]")).click();
-
-        WebElement vendor = driver.findElement(By.xpath("//*[.='java.specification.vendor']"));
-        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", vendor);
-        String javaVendor = driver.findElement(By.xpath("//*[.='java.specification.vendor']/following-sibling::td/div[2]")).getText();
-
-        Assert.assertEquals(javaVendor, "Oracle Corporation");
-    }
-
-    @Test
-    public void testShowEnviromentalVariables() {
-        WebDriver driver = getDriver();
-
-        driver.findElement(By.xpath("//*[@id=\"tasks\"]/div[3]")).click();
-        driver.findElement(By.xpath("//*[@id=\"main-panel\"]/section[4]/div/div[1]")).click();
-        driver.findElement(By.xpath("//*[@id=\"main-panel\"]/div[2]/div[2]/a")).click();
-
-        WebElement example = driver.findElement(By.xpath("//*[@id=\"main-panel\"]/div[4]/table/tbody/tr/td/div[1]"));
-        String infoHiddenClass = example.getDomAttribute(("class"));
-
-        driver.findElement(By.xpath("//*[@id=\"main-panel\"]/div[4]/div/button[1]")).click();
-
-        String infoShownClass = example.getDomAttribute(("class"));
-
-        Assert.assertEquals(infoHiddenClass, "app-hidden-info-reveal");
-        Assert.assertEquals(infoShownClass, "app-hidden-info-reveal jenkins-hidden");
-    }
-
-    @Test
-    public void testPluginsInfo() {
-        WebDriver driver = getDriver();
-
-        driver.findElement(By.xpath("//*[@id=\"tasks\"]/div[3]")).click();
-        driver.findElement(By.xpath("//*[@id=\"main-panel\"]/section[4]/div/div[1]")).click();
-        driver.findElement(By.xpath("//*[@id=\"main-panel\"]/div[2]/div[3]/a")).click();
-
-        String plugin1 = driver.findElement(By.xpath("//*[@id=\"main-panel\"]/div[5]/table/tbody/tr[1]/td[2]")).getText();
-        String plugin2 = driver.findElement(By.xpath("//*[@id=\"main-panel\"]/div[5]/table/tbody/tr[2]/td[2]")).getText();
-
-        Assert.assertNotNull(plugin1);
-        Assert.assertNotNull(plugin2);
-    }
 }
 
