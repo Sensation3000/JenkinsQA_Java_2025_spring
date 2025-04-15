2a07d5147d4f95477db090b6cfb24e3f52ed3936
Tyutyakova <p-v-a19@mail.ru>
2025-04-15 14:57:04 +0300

    fix er_18.006.03

--- src/test/java/school/redrover/FreestyleProjectManagementBuildNowTest.java
+++ src/test/java/school/redrover/FreestyleProjectManagementBuildNowTest.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
 import school.redrover.common.TestUtils;
