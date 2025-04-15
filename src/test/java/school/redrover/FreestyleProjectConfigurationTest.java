9235573fc4e6029767203f47cac463479f245e73
AlevtinaSemeniuk <alewtinni@gmail.com>
2025-04-15 11:15:40 -0700

    refactor FreestyleProjectConfigurationTest > testEnableProject https://github.com/RedRoverSchool/JenkinsQA_Java_2025_spring/issues/1210

--- src/test/java/school/redrover/FreestyleProjectConfigurationTest.java
+++ src/test/java/school/redrover/FreestyleProjectConfigurationTest.java
@@ -3,7 +3,6 @@ package school.redrover;
 import org.openqa.selenium.By;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
 import school.redrover.common.TestUtils;
@@ -21,14 +20,13 @@ public class FreestyleProjectConfigurationTest extends BaseTest {
         Assert.assertTrue(projectIsDisabledText.contains("This project is currently disabled"));
     }
 
-    @Ignore
     @Test
     public void testEnableProject() {
         TestUtils.createFreestyleProject(getDriver(), "Freestyle");
 
         getWait5().until(ExpectedConditions.elementToBeClickable(By.cssSelector("label[for='enable-disable-project']"))).click();
-        getDriver().findElement(By.name("Submit")).click();
-        getDriver().findElement(By.xpath("//button[contains(text(),'Enable')]")).click();
+        getWait5().until(ExpectedConditions.elementToBeClickable((By.name("Submit")))).click();
+        getWait5().until(ExpectedConditions.elementToBeClickable(By.xpath("//button[contains(text(),'Enable')]"))).click();
         getWait5().until(ExpectedConditions.refreshed(
                 ExpectedConditions.elementToBeClickable(By.cssSelector("[href$='configure']")))).click();
 
