6d2e827bd84db587b80d8846410eebbc279c070d
Margarita Shlykova <margarita.shlykova@medm.com>
2025-04-14 23:33:51 -0400

    TC_02.002.02 | Freestyle Project Configuration> Project Description > Check Description can be empty

--- src/test/java/school/redrover/FreestyleProjectConfigurationDescriptionTest.java
+++ src/test/java/school/redrover/FreestyleProjectConfigurationDescriptionTest.java
@@ -1,10 +1,12 @@
 package school.redrover;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
+import school.redrover.common.TestUtils;
 
 public class FreestyleProjectConfigurationDescriptionTest extends BaseTest {
 
@@ -27,5 +29,23 @@ public class FreestyleProjectConfigurationDescriptionTest extends BaseTest {
                         getDriver().findElement(By.id("notification-bar")))).isDisplayed());
         Assert.assertEquals(
                 getDriver().findElement(By.name("description")).getDomProperty("value"), DESCRIPTION_TEXT);
+    }
+
+    @Test
+    public void testDescriptionCanBeEmpty() {
+        WebDriver driver = getDriver();
+        String PROJECT_NAME = "Freestyle Project";
+
+        TestUtils.createFreestyleProject(driver, PROJECT_NAME);
+        driver.navigate().to("http://localhost:8080/job/" + PROJECT_NAME + "/configure");
+        getWait5().until(ExpectedConditions.visibilityOf(
+                driver.findElement(By.xpath("//textarea[@name='description']")))).clear();
+        driver.findElement(By.name("Apply")).click();
+
+        Assert.assertTrue(
+                getWait5().until(ExpectedConditions.visibilityOf(
+                        getDriver().findElement(By.id("notification-bar")))).isDisplayed());
+        Assert.assertTrue(
+                driver.findElement(By.xpath("//textarea[@name='description']")).getText().isBlank());
     }
 }
