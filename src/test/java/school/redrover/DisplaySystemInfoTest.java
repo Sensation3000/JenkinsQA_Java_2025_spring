cb07f6e2b28f579c62e9da603afed06501fe762e
Senina Olga <senina@reshenie-soft.ru>
2025-04-16 09:35:24 +0300

    added new test testShowSystemProperties to DisplaySystemInfoTest

--- src/test/java/school/redrover/DisplaySystemInfoTest.java
+++ src/test/java/school/redrover/DisplaySystemInfoTest.java
@@ -22,5 +22,20 @@ public class DisplaySystemInfoTest extends BaseTest {
         Assert.assertEquals(manageJ, "Manage Jenkins");
         Assert.assertEquals(systemSet, "System Information");
     }
+
+    @Test
+    public void testShowSystemProperties() {
+        WebDriver driver = getDriver();
+
+        driver.findElement(By.xpath("//*[@id=\"tasks\"]/div[3]")).click();
+        driver.findElement(By.xpath("//*[@id=\"main-panel\"]/section[4]/div/div[1]")).click();
+        driver.findElement(By.xpath("//*[@id=\"main-panel\"]/div[3]/div/button[1]")).click();
+
+        WebElement vendor = driver.findElement(By.xpath("//*[.='java.specification.vendor']"));
+        ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", vendor);
+        String javaVendor = driver.findElement(By.xpath("//*[.='java.specification.vendor']/following-sibling::td/div[2]")).getText();
+
+        Assert.assertEquals(javaVendor, "Oracle Corporation");
+    }git
 }
 
