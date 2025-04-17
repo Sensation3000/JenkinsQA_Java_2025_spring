73a5d0b07cbd4a5c20e0fa3e8568f09dcbd1bc12
Margarita Shlykova <margarita.shlykova@medm.com>
2025-04-16 20:41:44 -0400

    ER_23.004.04 | OrganizationFolderTest > testCancelFolderDeletion
    https://github.com/RedRoverSchool/JenkinsQA_Java_2025_spring/issues/1262

--- src/test/java/school/redrover/OrganizationFolderTest.java
+++ src/test/java/school/redrover/OrganizationFolderTest.java
@@ -2,17 +2,20 @@ package school.redrover;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
 
-public class OrganisationFolderTest extends BaseTest {
+public class OrganizationFolderTest extends BaseTest {
+
+    private static final String ORGANIZATION_FOLDER_NAME = "Organization_Folder2";
 
     @Test
     public void testCreateOrganizationFolderWithDefaultIcon() {
         getDriver().findElement(By.cssSelector("[href$='/newJob']")).click();
-        getDriver().findElement(By.id("name")).sendKeys("Organization_Folder");
+        getDriver().findElement(By.id("name")).sendKeys(ORGANIZATION_FOLDER_NAME);
 
         JavascriptExecutor js = (JavascriptExecutor) getDriver();
         js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
@@ -30,5 +33,16 @@ public class OrganisationFolderTest extends BaseTest {
 
         String organizationFolderCurrentIcon = getDriver().findElement(By.cssSelector("h1 > svg")).getAttribute("title");
         Assert.assertEquals(organizationFolderCurrentIcon, "Folder");
+    }
+
+    @Test (dependsOnMethods = "testCreateOrganizationFolderWithDefaultIcon")
+    public void testCancelFolderDeletion(){
+        getDriver().findElement(By.xpath("//td/a[@href='job/" + ORGANIZATION_FOLDER_NAME + "/']")).click();
+        getDriver().findElement(By.xpath("//a[@data-title='Delete Organization Folder']")).click();
+        getDriver().findElement(By.xpath("//button[@data-id='cancel']")).click();
+
+        Assert.assertEquals(
+                getWait5().until(ExpectedConditions.visibilityOf(
+                        getDriver().findElement(By.xpath("//*[@id='main-panel']/h1")))).getText(), ORGANIZATION_FOLDER_NAME);
     }
 }
