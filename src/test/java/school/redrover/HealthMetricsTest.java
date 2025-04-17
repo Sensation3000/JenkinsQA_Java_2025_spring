4695b1d49f0af8d3e807d4c8faa603dda19e4654
LizaAgeeva <lizaageeva1996@mail.ru>
2025-04-17 21:10:41 +0400

    Add Test 05.002.01 #1280 2

--- src/test/java/school/redrover/HealthMetricsTest.java
+++ src/test/java/school/redrover/HealthMetricsTest.java
@@ -16,16 +16,16 @@ public class HealthMetricsTest extends BaseTest {
     public void  checkAvailabilityHealthMetrics(){
         WebDriver driver = getDriver();
 
-        getWait5().until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"tasks\"]/div[1]/span/a"))).click();
-        driver.findElement(By.xpath("//*[@id=\"name\"]")).sendKeys("Test Folder");
-        WebElement element = driver.findElement(By.xpath("//*[@id=\"j-add-item-type-nested-projects\"]/ul/li[1]"));
+        getWait5().until(ExpectedConditions.presenceOfElementLocated(By.xpath("//a[@href='/view/all/newJob']"))).click();
+        driver.findElement(By.xpath("//*[@id='name']")).sendKeys("Test Folder");
+        WebElement element = driver.findElement(By.className("com_cloudbees_hudson_plugins_folder_Folder"));
         ((JavascriptExecutor) driver).executeScript("arguments[0].scrollIntoView(true);", element);  // Скроллим к элементу
         element.click();
-        driver.findElement(By.xpath("//*[@id=\"ok-button\"]")).click();
+        driver.findElement(By.xpath("//*[@id='ok-button']")).click();
 
-        getWait5().until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id=\"side-panel\"]/div[1]/div[1]/h1")));
-        driver.findElement(By.xpath("//*[@id=\"tasks\"]/div[2]/span/button"));
-        WebElement titleHM = driver.findElement(By.xpath("//*[@id=\"health-metrics\"]"));
+        getWait5().until(ExpectedConditions.presenceOfElementLocated(By.xpath("//*[@id='side-panel']/div[1]/div[1]/h1")));
+        driver.findElement(By.cssSelector("button[data-section-id='health-metrics']")).click();
+        WebElement titleHM = driver.findElement(By.xpath("//*[@id='health-metrics']"));
         Assert.assertEquals(titleHM.getText(), "Health metrics");
     }
 
