dc7936d2d6ca55bb3a1df9aaa38dbb0ecd3f7af9
Valentin.P <val.podkova@gmail.com>
2025-04-18 19:14:29 +0300

    RF_03.002.02 |Pipeline Configuration > Project Description > Create a project with a description

--- src/test/java/school/redrover/PipelineTest.java
+++ src/test/java/school/redrover/PipelineTest.java
@@ -1,32 +1,32 @@
 package school.redrover;
 
-import org.openqa.selenium.By;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
+import school.redrover.page.HomePage;
+import school.redrover.page.PipelineProjectPage;
 
 public class PipelineTest extends BaseTest {
     private static final String PROJECT_NAME = "New Pipeline";
-    private static final String PIPELINE_DESCRIPTION = "Pipeline Project Name" ;
+    private static final String PIPELINE_DESCRIPTION = "Pipeline Project Name";
 
     @Test
-    public void testPipeline() {
-        getDriver().findElement(By.xpath("//a[@href = '/view/all/newJob']")).click();
-        getDriver().findElement(By.id("name")).sendKeys(PROJECT_NAME);
-        getDriver().findElement(By.className("org_jenkinsci_plugins_workflow_job_WorkflowJob")).click();
-        getDriver().findElement(By.id("ok-button")).click();
-        getDriver().findElement(By.cssSelector("button[name='Submit']")).click();
+    public void testCreatePipeline() {
 
-        Assert.assertEquals(getDriver().findElement(By.xpath("//div[@class = 'jenkins-app-bar__content jenkins-build-caption']/h1")).getText(), PROJECT_NAME);
-    }
+        PipelineProjectPage pipelineProjectPage = new HomePage(getDriver())
+                .createJob()
+                .sendItemName(PROJECT_NAME)
+                .selectPipelineAndClickOk()
+                .sendDescription(PIPELINE_DESCRIPTION)
+                .clickSave();
 
-    @Test
-    public void testDescriptionPipeline() {
-        getDriver().findElement(By.xpath("//a[@href='editDescription']")).click();
-        getDriver().findElement(By.xpath("//textarea[@name='description']")).sendKeys(PIPELINE_DESCRIPTION);
-        getDriver().findElement(By.xpath("//div[@id = 'description']//button[@name='Submit']")).click();
-        String savedDescription = getDriver().findElement(By.xpath("//div[@id = 'description']/div[1]")).getText();
-
-        Assert.assertEquals(savedDescription, PIPELINE_DESCRIPTION);
+        Assert.assertTrue(pipelineProjectPage.getProjectName().contains(PROJECT_NAME),
+                "The project name does not contain the expected value");
+        Assert.assertTrue(pipelineProjectPage.getDescription().contains(PIPELINE_DESCRIPTION),
+                "Project description does not contain expected value");
     }
 }
+
+
+
+
