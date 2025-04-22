449dc0557246ede639e64593b943cf8ccb35dde5
Vadym Tymeichuk <40869955+Vadym11@users.noreply.github.com>
2025-04-22 19:01:08 +0100

    fix import

--- src/test/java/school/redrover/page/FreestyleProjectPage.java
+++ src/test/java/school/redrover/page/FreestyleProjectPage.java
@@ -1,6 +1,8 @@
 package school.redrover.page;
 
-import org.openqa.selenium.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import school.redrover.common.BasePage;
 import school.redrover.common.TestUtils;
