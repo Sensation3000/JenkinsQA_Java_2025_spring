761a503ef230ef63a2b0ab9b7f7139ee777c6350
viktoriak12 <viktoria.k12@mail.ru>
2025-04-23 19:58:30 +0300

    TC_01.001.12 | New Item > Creatе a new item > Create Pipeline #665

--- src/test/java/school/redrover/Pipeline2Test.java
+++ src/test/java/school/redrover/Pipeline2Test.java
@@ -6,6 +6,8 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
 import school.redrover.common.TestUtils;
+import school.redrover.page.HomePage;
+import school.redrover.page.PipelineProjectPage;
 
 public class Pipeline2Test extends BaseTest {
 
@@ -13,15 +15,13 @@ public class Pipeline2Test extends BaseTest {
     public void testCreateNewPipeline() {
         final String projectName = "PipelineProjectNameTest";
 
-        WebDriver driver = getDriver();
+        PipelineProjectPage pipelineProjectPage = new HomePage(getDriver())
+                .createJob()
+                .sendItemName(projectName)
+                .selectPipelineAndClickOk()
+                .clickSave();
 
-        driver.findElement(By.xpath("//*[@id=\"tasks\"]/div[1]/span/a")).click();
-        driver.findElement(By.className("jenkins-input")).sendKeys(projectName);
-        driver.findElement(By.className("org_jenkinsci_plugins_workflow_job_WorkflowJob")).click();
-        driver.findElement(By.id("ok-button")).click();
-        driver.findElement(By.name("Submit")).click();
-
-        Assert.assertEquals(driver.findElement(By.className("job-index-headline")).getText(), projectName);
+        Assert.assertEquals(pipelineProjectPage.getProjectName(), projectName);
     }
 
     @Test
