e2ba3a10f3e614095b25534e80f8b340fee97997
kuletsky <kuletsky@gmail.com>
2025-04-24 17:13:16 -0400

    RF_15.001.04 | User > Create User > Validation of the userName

--- src/test/java/school/redrover/component/HeaderComponent.java
+++ src/test/java/school/redrover/component/HeaderComponent.java
@@ -7,6 +7,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import school.redrover.common.BaseComponent;
 import school.redrover.page.HomePage;
+import school.redrover.page.SearchPage;
 
 public class HeaderComponent extends BaseComponent {
 
@@ -20,10 +21,19 @@ public class HeaderComponent extends BaseComponent {
 
         return new HomePage(getDriver());
     }
+
     public HomePage goToHomePage() {
         WebElement logo = getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.id("jenkins-home-link")));
         ((JavascriptExecutor) getDriver()).executeScript("arguments[0].click();", logo);
         getWait10().until(ExpectedConditions.visibilityOfElementLocated(By.id("main-panel")));
         return new HomePage(getDriver());
+    }
+
+    public SearchPage clickSearchButton() {
+        getWait10().until(
+                        ExpectedConditions.visibilityOfElementLocated(By.id("button-open-command-palette")))
+                .click();
+
+        return new SearchPage(getDriver());
     }
 }
