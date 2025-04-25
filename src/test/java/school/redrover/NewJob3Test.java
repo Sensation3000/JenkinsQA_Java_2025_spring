30d25d6654ada65910e968693716461536e83df6
DmKlv <dmklvd@gmail.com>
2025-04-25 07:26:23 +0200

    ref-pom-tests-v2

--- src/test/java/school/redrover/NewJob3Test.java
+++ src/test/java/school/redrover/NewJob3Test.java
@@ -1,10 +1,6 @@
 package school.redrover;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
 import school.redrover.common.TestUtils;
