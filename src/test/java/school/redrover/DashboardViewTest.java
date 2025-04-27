ad54dc6f3a8b9627eb18d8e808ab6e11abcc5670
Pasha Shalnov <pavelshalnov@gmail.com>
2025-04-27 10:56:15 +0300

    1. Add newViewPage and EditViewPage 2. Add clickNewView() and getNameOfView() methods to HomePage 3. Write a test for creating new View of MyView type
    TC_12.007.01 | Dashboard with the items > Customize Job Display > Create new My View
    #1526

--- src/test/java/school/redrover/DashboardViewTest.java
+++ src/test/java/school/redrover/DashboardViewTest.java
@@ -1,7 +1,6 @@
 package school.redrover;
 
 import org.testng.annotations.Test;
-import school.redrover.common.BasePage;
 import school.redrover.common.BaseTest;
 import school.redrover.page.HomePage;
 
