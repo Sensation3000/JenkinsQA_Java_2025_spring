04b4459337445944549f721e630480323002354c
AnnaLimutina <zip1061@mail.ru .>
2025-04-30 13:35:40 -0500

    RF_01.004.03 | FolderTest > testCreateWithValidName: testCreateWithValidName was changed to POM, testCreateFolderWithBlankConfiguration was deleted due to duplication,

--- src/test/java/school/redrover/FolderTest.java
+++ src/test/java/school/redrover/FolderTest.java
@@ -18,45 +18,27 @@ public class FolderTest extends BaseTest {
 
     private static final String FOLDER_NAME = "ProjectFolder";
 
-    @Ignore
     @Test(dataProvider = "projectNames", dataProviderClass = TestDataProvider.class)
     public void  testCreateWithValidName(String folderName) {
 
-        TestUtils.createFolder(getDriver(), folderName);
-        TestUtils.gotoHomePage(this);
-
-        List<WebElement> jobs = getDriver().findElements(By.xpath("//tr[contains(@id, 'job')]//a"));
+        List<String> jobs = new HomePage(getDriver())
+                .clickNewItemOnLeftSidePanel()
+                .sendItemName(folderName)
+                .selectFolderAndClickOk()
+                .saveAndReturnToHomePage()
+                .getProjectNameList();
 
         Assert.assertEquals(jobs.size(), 1);
-        Assert.assertEquals(jobs.get(0).getText(), folderName);
+        Assert.assertEquals(jobs.get(0), folderName);
     }
 
     @Test
-    public void testCreateFolderWithBlankConfiguration () {
-        getDriver().findElement(By.linkText("New Item")).click();
-        getDriver().findElement(By.id("name")).sendKeys(FOLDER_NAME);
-        getDriver().findElement(By.xpath("//span[text() = 'Folder']")).click();
-        getDriver().findElement(By.id("ok-button")).click();
-        getWait5().until(ExpectedConditions.visibilityOf(getDriver().findElement(By.name("Submit")))).click();
-
-        Assert.assertEquals(
-                getDriver().findElement(By.xpath("//div[@id='main-panel']/h1")).getText(), FOLDER_NAME);
-
-        TestUtils.gotoHomePage(this);
-
-        Assert.assertEquals(
-                getDriver().findElement(By.cssSelector(".jenkins-table__link > span:nth-child(1)")).getText(),
-                FOLDER_NAME);
-    }
-
-    @Test
-    public void testDisplayName() {
-        final String folderName = "My Super Cool Folder";
+    public void testCreateWithDisplayName() {
         final String expectedDisplayName = "Folder Display Name";
 
         String actualDisplayName = new HomePage(getDriver())
                 .clickNewItemOnLeftSidePanel()
-                .sendItemName(folderName)
+                .sendItemName(FOLDER_NAME)
                 .selectFolderAndClickOk()
                 .sendDisplayName(expectedDisplayName)
                 .clickSave()
@@ -67,12 +49,11 @@ public class FolderTest extends BaseTest {
 
     @Test
     public void testIfCopyFromFieldAppears() {
-        final String folderName = "My Super Cool Folder";
         final String expectedText = "Copy from";
 
         String copyFromFieldText = new HomePage(getDriver())
                 .clickNewItemOnLeftSidePanel()
-                .sendItemName(folderName)
+                .sendItemName(FOLDER_NAME)
                 .selectFolderAndClickOk()
                 .getHeader()
                 .clickLogoIcon()
@@ -83,7 +64,7 @@ public class FolderTest extends BaseTest {
     }
 
     @Test
-    public void testLinkProject () {
+    public void testCreateWithDescription () {
         final String descriptionName = "folder";
 
         FolderProjectPage folderProjectPage = new HomePage(getDriver())
