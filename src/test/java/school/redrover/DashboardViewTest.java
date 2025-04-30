3377b94f88d712bcfd46969c7955d4a6e12627c2
Pasha Shalnov <pavelshalnov@gmail.com>
2025-04-30 22:29:27 +0300

    DashboardViewTest: - add testCreateListView
    HomePage:
    - add isJobDisplayed - method for checking elements on dashboard
    
    EditViewPage:
    - add methods for testing basic positive test of creation New List View
    
    TC_12.007.01 | Dashboard with the items > Customize Job Display > Create new List View

--- src/test/java/school/redrover/DashboardViewTest.java
+++ src/test/java/school/redrover/DashboardViewTest.java
@@ -3,38 +3,66 @@ package school.redrover;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
 import school.redrover.page.HomePage;
+import school.redrover.page.view.EditViewPage;
+import school.redrover.page.view.NewViewPage;
+import java.util.List;
 
 import static org.testng.Assert.assertEquals;
+import static org.testng.Assert.assertTrue;
 
 
 public class DashboardViewTest extends BaseTest {
 
-    private static final String JOB_NAME = "Test item";
-    private final String VIEW_NAME = "TestViewName";
+    private final String MY_VIEW_NAME = "TestMyViewName";
+    private final String List_VIEW_NAME = "TestListViewName";
+    private static final String TEST_ITEM_JOB = "Test item";
 
     @Test(dependsOnMethods = "testCreateFreestyleProjectForView")
     public void testCreateMyView() {
-        new HomePage(getDriver())
-                .clickNewView()
-                .addName(VIEW_NAME)
+        new HomePage(getDriver()).clickNewView().addName(MY_VIEW_NAME)
                 .clickMyView()
                 .clickCreateButton();
 
-        String newViewName = new HomePage(getDriver()).getNameOfView(VIEW_NAME);
-        assertEquals(newViewName, VIEW_NAME);
+        List<String> projectNameList = new HomePage(getDriver()).getProjectNameList();
+        assertEquals(projectNameList.size(), 1);
+        assertEquals(new HomePage(getDriver()).getNameOfView(MY_VIEW_NAME), MY_VIEW_NAME);
+    }
+
+    @Test(dependsOnMethods = "testCreateFreestyleProjectForView")
+    public void testCreateListView() {
+        NewViewPage initialPage = new HomePage(getDriver()).clickNewView().addName(List_VIEW_NAME);
+
+        EditViewPage editViewPage = (EditViewPage) initialPage
+                .clickListView()
+                .clickCreateButton();
+
+        editViewPage
+                .fillDescription("Description for Test List View")
+                .JobsCheckTestItem(TEST_ITEM_JOB)
+                .clickAddJobFilter()
+                .clickStatusFilterOfJobFilter()
+                .clickAddColumn()
+                .checkProjectDescriptionColumn()
+                .clickSaveButton();
+
+        List<String> projectNameList = new HomePage(getDriver()).getProjectNameList();
+        assertEquals(projectNameList.size(), 1);
+        assertTrue(new HomePage(getDriver()).isJobDisplayed(TEST_ITEM_JOB));
+        assertEquals(new HomePage(getDriver()).getNameOfView(List_VIEW_NAME), List_VIEW_NAME);
     }
 
     @Test
     public void testCreateFreestyleProjectForView() {
         String projectName = new HomePage(getDriver())
                 .clickNewItem()
-                .sendItemName(JOB_NAME)
+                .sendItemName(TEST_ITEM_JOB)
                 .selectFreestyleClickOkAndWaitCreateItemFormIsClose()
                 .waitUntilTextConfigureToBePresentInH1()
                 .clickSaveButton()
-                .waitUntilTextNameProjectToBePresentInH1(JOB_NAME)
+                .waitUntilTextNameProjectToBePresentInH1(TEST_ITEM_JOB)
                 .getProjectName();
 
-        assertEquals(projectName, JOB_NAME);
+        assertEquals(projectName, TEST_ITEM_JOB);
     }
+
 }
--- src/test/java/school/redrover/page/view/EditViewPage.java
+++ src/test/java/school/redrover/page/view/EditViewPage.java
@@ -1,12 +1,85 @@
 package school.redrover.page.view;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import school.redrover.common.BasePage;
+import school.redrover.page.HomePage;
 
 public class EditViewPage extends BasePage {
 
     public EditViewPage(WebDriver driver) {
         super(driver);
+    }
+    public JavascriptExecutor jsExecutor = (JavascriptExecutor) getDriver();
+
+    public String getName() {
+        return getDriver().findElement(By.xpath(
+                "//*[@id=\"main-panel\"]/form/div[1]/section[1]/div[1]/div[2]/input")).getText();
+    }
+
+    public EditViewPage fillDescription(String description) {
+        getDriver().findElement(By.xpath("//*[@id=\"main-panel\"]/form/div[1]/section[1]/div[2]/div[3]/textarea"))
+                .sendKeys(description);
+
+        return this;
+    }
+
+    public EditViewPage JobsCheckTestItem(String jobName) {
+        findElementContainsTextAndClick("label", jobName, this.getClass());
+
+        return this;
+    }
+
+    public EditViewPage clickAddJobFilter() {
+        findElementContainsTextAndClick("button", "Add Job Filter", EditViewPage.class);
+
+        return this;
+    }
+
+    public EditViewPage clickStatusFilterOfJobFilter() {
+        findElementContainsTextAndClick("button","Status Filter", EditViewPage.class);
+
+        return this;
+    }
+
+    public EditViewPage clickAddColumn() {
+       findElementContainsTextAndClick("button", "Add column", EditViewPage.class);
+
+       return this;
+    }
+
+    public EditViewPage checkProjectDescriptionColumn() {
+        findElementContainsTextAndClick("button", "\n" +
+                "          \n" +
+                "          Project description\n" +
+                "                    \n" +
+                "          \n" +
+                "      ", EditViewPage.class);
+
+        return this;
+    }
+
+    public HomePage clickSaveButton() {
+        getDriver().findElement(By.name("Submit")).click();
+
+        return new HomePage(getDriver());
+    }
+
+    private <T extends BasePage> void findElementContainsTextAndClick(String element, String text, Class<T> clazz) {
+        WebElement el = getWait5().until(ExpectedConditions.presenceOfElementLocated(
+                By.xpath("//" + element + "[contains(text(),'" + text + "')]")));
+        jsExecutor.executeScript("arguments[0].scrollIntoView({block: 'center'});", el);
+        getWait5().until(ExpectedConditions.elementToBeClickable(el));
+        jsExecutor.executeScript("arguments[0].click();", el);
+
+        try {
+            clazz.getDeclaredConstructor(WebDriver.class).newInstance(getDriver());
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to instantiate page object: " + clazz.getName(), e);
+        }
     }
 
 }
