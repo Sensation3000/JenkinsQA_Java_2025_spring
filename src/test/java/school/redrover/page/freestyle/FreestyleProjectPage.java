0bb04ee16d89d66c3edc5d4f33f4bb92d85d3080
Margarita Shlykova <margarita.shlykova@medm.com>
2025-05-02 12:46:45 -0400

    Update test to POM

--- src/test/java/school/redrover/page/freestyle/FreestyleProjectPage.java
+++ src/test/java/school/redrover/page/freestyle/FreestyleProjectPage.java
@@ -45,6 +45,21 @@ public class FreestyleProjectPage extends BasePage {
         return this;
     }
 
+    public FreestyleProjectPage clickPreviewDescription() {
+        getDriver().findElement(By.className("textarea-show-preview")).click();
+
+        return this;
+    }
+
+    public boolean isPreviewDescriptionBlockDisplayed() {
+        return getWait5().until(ExpectedConditions.visibilityOf(getDriver()
+                .findElement(By.className("textarea-preview")))).isDisplayed();
+    }
+
+    public String getTextFromPreviewDescriptionBlock() {
+        return getDriver().findElement(By.className("textarea-preview")).getText();
+    }
+
     public FreestyleProjectPage clickSave() {
         getDriver().findElement(By.name("Submit")).click();
 
