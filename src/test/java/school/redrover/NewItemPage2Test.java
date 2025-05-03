66c041528a1a04ea5cdfda717be88677004bf4f7
StepanidaKirillina1 <105031048+StepanidaKirillina1@users.noreply.github.com>
2025-05-03 10:08:41 +0900

    RF_01.004.14 | NewItemPage2Test > testIfDifferentProjectsHaveDifferentSvgIcons (#1719)

--- src/test/java/school/redrover/NewItemPage2Test.java
+++ src/test/java/school/redrover/NewItemPage2Test.java
@@ -11,19 +11,10 @@ import school.redrover.page.newitem.NewItemPage;
 import school.redrover.testdata.TestDataProvider;
 
 import java.util.List;
-import java.util.Random;
-
 
 public class NewItemPage2Test extends BaseTest {
     HomePage homePage;
     String projectName;
-
-    public static int getRandomNumber() {
-        int[] numbers = {1, 2, 3, 5, 6};
-        Random random = new Random();
-
-        return numbers[random.nextInt(numbers.length)];
-    }
 
     @BeforeMethod
     void setUp() {
@@ -221,7 +212,9 @@ public class NewItemPage2Test extends BaseTest {
 
     @Test(dependsOnMethods = "createNewFolderProject")
     public void testIfDifferentProjectsHaveDifferentSvgIcons() {
-        TestUtils.newItemCreate(this, TestUtils.generateRandomAlphanumeric(), getRandomNumber());
+        TestUtils.newItemCreate(this,
+                                         TestUtils.generateRandomAlphanumeric(),
+                                         TestUtils.getRandomNumberBetween1And6(1, 2, 3, 5, 6));
 
         Assert.assertFalse(homePage.isSvgIconDifferentBetweenProjects());
     }
