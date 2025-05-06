ea2ae40173a5e483b679b9620f90b81551be998b
Margarita Shlykova <margarita.shlykova@medm.com>
2025-05-06 00:29:19 -0400

    Update test and move to another class

--- src/test/java/school/redrover/DashboardTest.java
+++ src/test/java/school/redrover/DashboardTest.java
@@ -33,9 +33,21 @@ public class DashboardTest extends BaseTest {
 
     @Test
     public void testPossibleToCreateJobFromDashboard() {
-        NewItemPage newItemPage = new HomePage(getDriver()).clickCreateJob();
+        NewItemPage newItemPage = new HomePage(getDriver())
+                .clickCreateJob();
 
         Assert.assertTrue(newItemPage.isNewItemPageOpened());
+    }
+
+    @Test
+    public void testCancelJobCreationFromDashboard() {
+        boolean isJobListEmpty = new HomePage(getDriver())
+                .clickCreateJob()
+                .getHeader()
+                .clickLogoIcon()
+                .isJobListEmpty();
+
+        Assert.assertTrue(isJobListEmpty);
     }
 
     @Test
