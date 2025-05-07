3462596312f002344a8a2413be426280055c62d5
Margarita Shlykova <margarita.shlykova@medm.com>
2025-05-07 11:41:51 -0400

    Fix test

--- src/test/java/school/redrover/OrganizationFolderTest.java
+++ src/test/java/school/redrover/OrganizationFolderTest.java
@@ -1,7 +1,6 @@
 package school.redrover;
 
 import org.testng.Assert;
-import org.testng.annotations.Ignore;
 import org.testng.annotations.Test;
 import school.redrover.common.BaseTest;
 import school.redrover.page.HomePage;
@@ -58,7 +57,6 @@ public class OrganizationFolderTest extends BaseTest {
         Assert.assertEquals(availableIcons, List.of("Default Icon", "Metadata Folder Icon"));
     }
 
-    @Ignore // OrganizationFolderTest.testSelectDefaultIconForOrganizationFolder:70 » StaleElementReference stale element reference: stale element not found
     @Test (dependsOnMethods = "testAvailableIconsForOrganizationFolder")
     public void testSelectDefaultIconForOrganizationFolder() {
         String orgFolderIconTitle = new HomePage(getDriver())
@@ -85,7 +83,6 @@ public class OrganizationFolderTest extends BaseTest {
                 newItemPage.getEmptyNameMessage(), "» This field cannot be empty, please enter a valid name");
     }
 
-    @Ignore
     @Test(dependsOnMethods = "testSelectDefaultIconForOrganizationFolder")
     public void testCancelOrganizationFolderDeletion(){
         String orgFolderPageHeader = new HomePage(getDriver())
@@ -104,7 +101,6 @@ public class OrganizationFolderTest extends BaseTest {
                 "Organization folder is deleted");
     }
 
-    @Ignore
     @Test (dependsOnMethods = "testCancelOrganizationFolderDeletion")
     public void testDeleteEmptyOrganizationFolderFromFolderPage() {
         OrganizationFolderPage orgFolderPage = new HomePage(getDriver())
