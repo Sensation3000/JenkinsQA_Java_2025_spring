26a950b11012d556ad74c0b076a4f9b10fb8c7ea
Margarita Shlykova <margarita.shlykova@medm.com>
2025-05-06 23:02:17 -0400

    Fix test

--- src/test/java/school/redrover/page/HomePage.java
+++ src/test/java/school/redrover/page/HomePage.java
@@ -41,7 +41,6 @@ public class HomePage extends BasePage {
 
     public HomePage(WebDriver driver) {
         super(driver);
-        PageFactory.initElements(driver,this);
     }
 
     public HomePage clickAddDescriptionButton() {
