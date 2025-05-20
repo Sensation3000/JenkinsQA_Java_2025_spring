b002cdc08981ae335d14173b03f2a718789fd570
Yaroslavasg <garmash01@gmail.com>
2025-05-19 23:59:36 -0400

    AC_05.004.02 | Folder Configuration > Save or Apply> Test if "Leave site? Changes you made.." appeared when user leave page without savings. #2034 Create component for SideMenuInsideFolder

---
+++ src/test/java/school/redrover/component/SideMenuInHomePageComponent.java
@@ -0,0 +1,19 @@
+package school.redrover.component;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import school.redrover.common.BaseComponent;
+import school.redrover.common.BasePage;
+
+public class SideMenuInHomePageComponent extends BaseComponent {
+
+    public SideMenuInHomePageComponent(WebDriver driver) {
+        super(driver);
+    }
+
+    public <T extends BasePage> T clickItemOnSidePanel(String itemName, T resultPage) {
+        getWait5().until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@id='tasks']//a[.//span[text()='" + itemName + "']]"))).click();
+        return resultPage;
+    }
+}
