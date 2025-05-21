2588f5d1d0042b679d959923bff4c716cc9a5ee6
Yaroslavasg <garmash01@gmail.com>
2025-05-21 16:35:28 -0400

    RF_refactoring BasePage Class

--- src/test/java/school/redrover/common/BasePage.java
+++ src/test/java/school/redrover/common/BasePage.java
@@ -2,6 +2,7 @@ package school.redrover.common;
 
 import org.openqa.selenium.WebDriver;
 import school.redrover.component.HeaderComponent;
+import school.redrover.component.SideMenuInFolderComponent;
 
 public abstract class BasePage extends BaseModel {
 
@@ -11,6 +12,10 @@ public abstract class BasePage extends BaseModel {
 
     public HeaderComponent getHeader() {
         return new HeaderComponent(getDriver());
+    }
+
+    public SideMenuInFolderComponent getSideMenuInFolder(){
+        return new SideMenuInFolderComponent(getDriver());
     }
 
     public String getCurrentUrl() {
