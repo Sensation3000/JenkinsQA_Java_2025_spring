956befc5f29ba1f6b1ad3c217ce56361d1273495
Roman Zemlianykh <r.zemlianykh@nakusi.games>
2025-05-26 12:27:27 +0700

    TC_22.005.01 | Multibranch Pipeline Management > View Multibranch Pipeline Events | Display all events per branch

--- src/test/java/school/redrover/page/multibranch/MultibranchProjectPage.java
+++ src/test/java/school/redrover/page/multibranch/MultibranchProjectPage.java
@@ -1,9 +1,6 @@
 package school.redrover.page.multibranch;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.JavascriptExecutor;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.support.FindAll;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -60,12 +57,19 @@ public class MultibranchProjectPage extends BasePage {
 
     public MultibranchProjectPage navigateToJobStatus(String jobName) {
         for (int i = 0; i < 3; i++) {
-
-            getWait10()
-                    .until(ExpectedConditions.elementToBeClickable(By.xpath("//a[text()='" + jobName + "']")))
-                    .click();
+            try {
+                WebElement jobLink = getWait10()
+                        .until(ExpectedConditions.elementToBeClickable(By.xpath("//a[text()='" + jobName + "']")));
+                jobLink.click();
+                return this;
+            } catch (StaleElementReferenceException e) {
+                try {
+                    Thread.sleep(500);
+                } catch (InterruptedException ignored) {}
+            }
         }
-        return this;
+
+        throw new RuntimeException("Failed to click job link after 3 attempts due to stale element");
     }
 
     public List<String> getAllBranchNames() throws InterruptedException {
