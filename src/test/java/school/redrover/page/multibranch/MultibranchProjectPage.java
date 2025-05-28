bd752dc011598cfa0357b9cfdcbdd0391d21d625
Roman Zemlianykh <r.zemlianykh@nakusi.games>
2025-05-28 17:43:29 +0700

    TC_22.005.01 | Multibranch Pipeline Management > View Multibranch Pipeline Events | Display all events per branch

--- src/test/java/school/redrover/page/multibranch/MultibranchProjectPage.java
+++ src/test/java/school/redrover/page/multibranch/MultibranchProjectPage.java
@@ -58,7 +58,7 @@ public class MultibranchProjectPage extends BasePage {
     }
 
     public MultibranchProjectPage navigateToJobStatus(String jobName) {
-        for (int i = 0; i < 3; i++) {
+        for (int i = 0; i < 5; i++) {
             WebElement jobLink = getWait10()
                     .until(ExpectedConditions.elementToBeClickable(By.xpath("//a[text()='" + jobName + "']")));
             jobLink.click();
