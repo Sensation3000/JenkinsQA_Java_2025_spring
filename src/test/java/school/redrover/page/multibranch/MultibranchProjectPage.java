3a233bf8c5a3325a5c9508ee0d8c4c2c520a654c
Roman Zemlianykh <r.zemlianykh@nakusi.games>
2025-05-29 08:51:16 +0700

    TC_22.005.01 | Multibranch Pipeline Management > View Multibranch Pipeline Events | Display all events per branch

--- src/test/java/school/redrover/page/multibranch/MultibranchProjectPage.java
+++ src/test/java/school/redrover/page/multibranch/MultibranchProjectPage.java
@@ -59,11 +59,18 @@ public class MultibranchProjectPage extends BasePage {
 
     public MultibranchProjectPage navigateToJobStatus(String jobName) {
         for (int i = 0; i < 5; i++) {
-            WebElement jobLink = getWait10()
-                    .until(ExpectedConditions.elementToBeClickable(By.xpath("//a[text()='" + jobName + "']")));
-            jobLink.click();
+            try {
+                getWait10()
+                        .until(ExpectedConditions
+                                .elementToBeClickable(By.xpath("//a[text()='" + jobName + "']"))).click();
+                return this;
+            } catch (WebDriverException e) {
+                try {
+                    Thread.sleep(500); // короткий сон
+                } catch (InterruptedException ignored) {}
+            }
         }
-        return this;
+        throw new WebDriverException("Failed to navigate to job status for: " + jobName);
     }
 
     public List<String> getAllBranchNames() {
